5950f470825f4cad0b6355d5279792a6ade1945f
Jerven Bolleman <jerven.bolleman@sib.swiss>
2017-04-10 21:02:22 +0200

    Merge remote-tracking branch 'origin/onesparqlquery'
    Conflicts:
    	dist/interaction-viewer.js
    	dist/interaction-viewer.min.js

--- packages/interaction-viewer/src/sparqlLoader.js
+++ packages/interaction-viewer/src/sparqlLoader.js
@@ -1,6 +1,7 @@
 const d3 = require('d3');
 const _ = require('underscore');
 const sparql_uniprot_org="http://sparql.uniprot.org/sparql/"
+
 const sparqlLoader = {
   order: function(accession, nodes) {
     // Always place the query accession at the top
@@ -9,25 +10,29 @@ const sparqlLoader = {
   isDuplicateEdge: function(edges, edge) {
     return _.find(edges, function(d) { return (d.source === edge.source && d.target === edge.target)});
   },
-  processData: function(accession, nodeData, edgeData) {
-
+  isDuplicateNode: function(nodes, node) {
+    return _.find(nodes, function(d) { return (d.accession=== node.accession)});
+  },
+  processData: function(accession, edgeData) {
     var json = {
       nodes: [],
       links: []
     };
 
-    for (var element of nodeData.results.bindings) {
+    for (var element of edgeData.results.bindings) {
       var node = {
-        'accession': element.accession.value,
-        'entryName': element.entry_name ? element.entry_name.value : element.accession.value,
-        'disease': element.has_disease.value == 1,
-        'subcell': element.has_subcell.value == 1
+        'accession': element.source.value,
+        'entryName': element.entry_name ? element.entry_name.value : element.source.value,
+        'diseases': element.has_disease !== null,
+        'subcell': element.has_subcell !== null
       };
-      json.nodes.push(node);
+      if (!this.isDuplicateNode(json.nodes, node)){
+	json.nodes.push(node);
+      }
     }
     this.order(accession, json.nodes);
     const accessions = _.pluck(json.nodes, 'accession');
-    for (element of edgeData.results.bindings) {
+    for (var element of edgeData.results.bindings) {
       var sourceIndex = _.indexOf(accessions, element.source.value);
       var targetIndex = _.indexOf(accessions, element.target.value);
       if ((sourceIndex >= 0) && (targetIndex >= 0)) {
@@ -49,97 +54,37 @@ const sparqlLoader = {
       return q;
   },
   loadData: function(entry) {
-    var promiseNodes = new Promise(function(resolve) {
-      var q=sparqlLoader.convertQuery(`PREFIX  uniprotkb: <http://purl.uniprot.org/uniprot/>
-PREFIX  :   <http://purl.uniprot.org/core/>
-SELECT 
-  DISTINCT  
-    (strafter(substr(str(?te), 32), "/") AS ?accession) 
-    ?entry_name 
-    (MAX(?disease_type) AS ?has_disease) 
-    (MAX(?subcell_type) AS ?has_subcell)
-FROM <http://sparql.uniprot.org/uniprot>
-WHERE
-{ 
-  BIND(uniprotkb:${entry} AS ?p)
-  {
-    BIND(uniprotkb:${entry} AS ?te)
-  }
-    UNION
-  { ?p  :interaction          ?i .
-    ?i  a                     :Non_Self_Interaction ;
-        :participant ?sp .
-    { ?i   :participant  ?tp .
-      ?sp  owl:sameAs    ?p .
-      ?tp  owl:sameAs    ?te .
-      FILTER ( ! sameTerm(?p, ?te) )
-    }
-      UNION
-    {
-      ?sp  owl:sameAs    ?se .
-      ?se  :interaction  ?i2 .
-      FILTER ( ! sameTerm(?i, ?i2) )
-      {
-        ?i2  a                     :Non_Self_Interaction ;
-             :participant          ?tp , ?sp .
-        ?tp  owl:sameAs            ?te .
-        ?te  :interaction          ?oi .
-        { ?oi :participant/owl:sameAs ?p } 
-        FILTER ( ( ( ! sameTerm(?se, ?p) ) && ( ! sameTerm(?se, ?te) ) ) && ( ! sameTerm(?te, ?p) ) )
-      }
-        UNION
-      { 
-        ?i2  a                     :Self_Interaction ;
-             :participant/owl:sameAs ?se .
-        BIND(?se AS ?te)
-      }
-    }
-  }
-  OPTIONAL {
-    ?te :mnemonic  ?entry_name .
-  } 
-  OPTIONAL {
-    ?te :annotation/a ?type
-  }
-  BIND(if(( ?type = :Disease_Annotation ), 1, 0) AS ?disease_type)
-  BIND(if(( ?type = :Subcellular_Location_Annotation ), 1, 0) AS ?subcell_type)
-}
-GROUP BY ?te ?entry_name
-ORDER BY ?entry_name`);
-      var url = sparql_uniprot_org + "?query=%23uuw%0A%0D"+q+"&format=srj";
-      d3.json(url, data => {
-        resolve(data);
-      });
-    });
     var promiseEdges = new Promise(function(resolve) {
       var q=sparqlLoader.convertQuery(`PREFIX :<http://purl.uniprot.org/core/>
 SELECT
-  (strafter(substr(str(?se), 32), "/") AS ?source) 
-  (strafter(substr(str(?te), 32), "/") AS ?target) 
-  (?e AS ?exp)
-  (substr(str(?sp), 32) AS ?source_intact) 
-  (substr(str(?tp), 32) AS ?target_intact)
+  DISTINCT
+    (strafter(substr(str(?se), 32), "/") AS ?source) 
+    (strafter(substr(str(?te), 32), "/") AS ?target) 
+    (?e AS ?exp)
+    (substr(str(?sp), 32) AS ?source_intact) 
+    (substr(str(?tp), 32) AS ?target_intact)
+    ?entry_name 
+    (GROUP_CONCAT(strafter(substr(str(?disease), 32), "/"); separator=',') AS ?diseases) 
+    (GROUP_CONCAT(strafter(substr(str(?location), 32), "/"); separator=',') AS ?locations) 
 FROM <http://sparql.uniprot.org/uniprot>
 WHERE
 { 
   BIND(<http://purl.uniprot.org/uniprot/${entry}> AS ?p)
-  BIND(:interaction AS ?ui)
-  { ?p  ?ui                   ?i .
+  { 
+    ?p  :interaction          ?i .
     ?i  a                     :Self_Interaction ;
         :experiments          ?e ;
-        :participant          ?sp ;
-        :participant          ?tp
+        :participant          ?sp , ?tp .
         BIND(?p AS ?se)
         BIND(?p AS ?te)
   } 
     UNION 
   { 
-    ?p  ?ui                   ?i .
+    ?p  :interaction          ?i .
     ?i  a                     :Non_Self_Interaction
     { 
         ?i   :experiments  ?e ;
-             :participant  ?sp ;
-             :participant  ?tp .
+             :participant  ?sp , ?tp .
         ?sp  owl:sameAs    ?p .
         ?tp  owl:sameAs    ?te
         FILTER ( ! sameTerm(?p, ?te) )
@@ -149,39 +94,46 @@ WHERE
     { 
         ?i   :participant  ?sp .
         ?sp  owl:sameAs    ?se .
-        ?se  ?ui           ?i2 .
+        ?se  :interaction  ?i2 .
         ?i2  :experiments  ?e
         FILTER ( ! sameTerm(?i, ?i2) )
         { 
             ?i2  a                     :Non_Self_Interaction ;
-                 :participant          ?tp ;
-                 :participant          ?sp .
+                 :participant          ?tp , ?sp .
             ?tp  owl:sameAs            ?te .
-            ?te ?ui ?oi . 
-            { ?oi :participant/owl:sameAs ?p } 
+            ?te :interaction/:participant/owl:sameAs ?p .
             FILTER ( ( ( ! sameTerm(?se, ?p) ) && ( ! sameTerm(?se, ?te) ) ) && ( ! sameTerm(?te, ?p) ) )
         }
            UNION
         {
             ?i2  a                     :Self_Interaction ;
                  :participant          ?tp .
-            ?tp  owl:sameAs            ?se
+            ?tp  owl:sameAs            ?se .
             BIND(?se AS ?te)
         }
-     }
-   }
+    }
+  }
+  OPTIONAL {
+    ?te :mnemonic ?entry_name .
+  } 
+  OPTIONAL {
+    ?te :annotation/:disease ?disease .
+  }
+  OPTIONAL {
+    ?te :annotation/:locatedIn/(:cellularComponent|:topology|:orientation) ?location .
+  }
 }
-GROUP BY ?se ?te ?e ?sp ?tp
-ORDER BY ?se ?te ?e ?sp ?tp`);
+GROUP BY ?entry_name ?se ?te ?e ?sp ?tp
+ORDER BY ?entry_name ?se ?te ?e ?sp ?tp
+`);
       var url = sparql_uniprot_org + "?query=%23uuw%0A%0D"+q+"&format=srj";
       d3.json(url, data => {
         resolve(data);
       });
     });
-    return Promise.all([promiseNodes, promiseEdges]).then(function(res) {
-      return sparqlLoader.processData(entry, res[0], res[1]);
+    return Promise.all([promiseEdges]).then(function(res) {
+      return sparqlLoader.processData(entry, res[0]);
     });
   }
-
 };
 module.exports = sparqlLoader;
