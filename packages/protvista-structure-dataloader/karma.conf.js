3b8a51789737a5d8cc8da6109a2dc0c27f317ad3
ljgarcia <ljgarcia@ebi.ac.uk>
2017-05-15 21:27:44 +0100

    Add mocha reporter with default options.

--- packages/protvista-structure-dataloader/karma.conf.js
+++ packages/protvista-structure-dataloader/karma.conf.js
@@ -38,14 +38,14 @@ module.exports = function(config) {
 
     plugins: [
         'karma-browserify',
-        'karma-mocha', 'karma-chai',
+        'karma-mocha', 'karma-chai', 'karma-mocha-reporter',
         'karma-firefox-launcher'
     ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -72,7 +72,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
