08473ac7890d7d4009582ab218f32470fbaf253e
ljgarcia <ljgarcia@ebi.ac.uk>
2017-05-15 22:59:04 +0100

    Rename Parser to Foo.

--- packages/protvista-structure-dataloader/test/ParserSpec.js
+++ packages/protvista-structure-dataloader/test/ParserSpec.js
@@ -1,24 +1,11 @@
-require('babel-core/register');
-
-var test = require('../src/Parser.js');
-// chai is an assertion library
-var chai = require('chai');
-
-// @see http://chaijs.com/api/assert/
-var assert = chai.assert;
-
-// register alternative styles
-// @see http://chaijs.com/api/bdd/
-var expect = chai.expect;
-var should = chai.should;
+import {Parser} from '../src/Parser';
+import {assert} from 'chai';
+import {expect} from 'chai';
 
 describe('Parser', () => {
-	it('should pass', function() {
-	    var test = true;
-	    assert.equal(test, true);
-	});
-
-	it('should see Parser', () => {
-	    test('aa', 'b', 22, 43);
-	});
+    it('should create a parser', () => {
+        const url = 'a url';
+        let aParser = new Parser(url);
+        expect(aParser.loaderURL).to.equal(url);
+    });
 });
