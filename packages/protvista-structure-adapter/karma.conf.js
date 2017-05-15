aba0c1de5b752ecc2206a06e7532b5e009133093
ljgarcia <ljgarcia@ebi.ac.uk>
2017-05-15 22:08:01 +0100

    Add code coverage.

--- packages/protvista-structure-adapter/karma.conf.js
+++ packages/protvista-structure-adapter/karma.conf.js
@@ -32,21 +32,24 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/**/*.js': ['browserify'],
+        'src/**/*.js': ['browserify', 'coverage'],
         'test/**/*Spec.js': ['browserify']
     },
 
     plugins: [
         'karma-browserify',
-        'karma-mocha', 'karma-chai', 'karma-mocha-reporter',
+        'karma-mocha', 'karma-chai', 'karma-mocha-reporter', 'karma-coverage',
         'karma-firefox-launcher'
     ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
-
+    reporters: ['mocha', 'coverage'],
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'text-summary'
+    },
 
     // web server port
     port: 9876,
