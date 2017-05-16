6cba8349663a851fd3aae3757c38683772b0846c
ljgarcia <ljgarcia@ebi.ac.uk>
2017-05-16 19:13:15 +0100

    Add handler class to config so it can be load on the fly.
    * Probably not useful now but later if we want to parse information from other/more sources.

--- packages/protvista-structure-adapter/src/Constants.js
+++ packages/protvista-structure-adapter/src/Constants.js
@@ -4,15 +4,24 @@
 let Config = require('./config.json');
 
 const webServices = Config.webServices;
+const defaultProvider = 'uniprot';
 const defaultWebServiceURL = 'https://www.ebi.ac.uk/proteins/api/proteins/';
 
 class Constants {
     static getWebServiceURL(provider) {
-        return webServices[provider] ? webServices[provider] : defaultWebServiceURL;
+        return webServices[provider] ? webServices[provider].url : defaultWebServiceURL;
     }
 
-    static isWebServiceURLDefault(url) {
-        return url === defaultWebServiceURL;
+    static getWebServiceHandler(provider) {
+        return webServices[provider] ? webServices[provider].handler : defaultWebServiceURL;
+    }
+
+    static getDefaultProvider() {
+        return defaultProvider;
+    }
+
+    static isValidProvider(provider) {
+        return webServices[provider] ? true : false;
     }
 }
 
