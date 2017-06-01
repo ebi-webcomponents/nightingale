0d60e11f0b6bcd30e8faef05c6ae4f19b0736e74
ljgarcia <ljgarcia@ebi.ac.uk>
2017-06-01 12:03:43 +0100

    Add data transformation function.

--- packages/protvista-structure-adapter/src/Parser.js
+++ packages/protvista-structure-adapter/src/Parser.js
@@ -1,10 +1,35 @@
 /*jslint node: true */
 "use strict";
 
-const Constants = require('./Constants');
 const _ = require('underscore');
+const Constants = require('./Constants');
 const ParserHelper = require('./ParserHelper');
 require('./UniProtEntryLoader');
+
+const transformData = (self, json) => {
+    self._parsedData.accession = self.accession;
+    self._parsedData.sequence = json.sequence.sequence;
+    self._parsedData.features = [];
+    const structures = _.filter(json.dbReferences, (reference) => {
+        return reference.type === 'PDB';
+    });
+    self._parsedData.features = _.map(structures, (structure) => {
+        const beginEnd = structure.properties.chains
+            ? ParserHelper.getBeginEnd(structure.properties.chains) : {begin: 0, end: 0};
+        return {
+            type: self.featureType,
+            category: self.featureCategory,
+            description: ParserHelper.getDescription(structure.properties),
+            begin: beginEnd.begin,
+            end: beginEnd.end,
+            xrefs: [{
+                name: 'PDB',
+                id: structure.id,
+                url: 'http://www.ebi.ac.uk/pdbe-srv/view/entry/' + structure.id
+            }]
+        };
+    });
+};
 
 class Parser {
     constructor(accession, provider) {
@@ -12,7 +37,7 @@ class Parser {
         this._provider = provider;
         this._featureType = "PDB_STRUCTURE";
         this._featureCategory = "STRUCTURAL";
-        this._parsedObject = {};
+        this._parsedData = {};
         if (!Constants.isValidProvider(this._provider)) {
             this._provider = Constants.getDefaultProvider();
         }
@@ -25,7 +50,8 @@ class Parser {
         let self = this;
         this._loader.retrieveEntryPromise()
             .then(function(json) {
-                console.log(json);
+                transformData(self, json);
+                console.log(self.parsedData);
             })
             .catch(function(error) {
                 console.log(error);
@@ -50,6 +76,10 @@ class Parser {
 
     get featureCategory() {
         return this._featureCategory;
+    }
+
+    get parsedData() {
+        return this._parsedData;
     }
 }
 
