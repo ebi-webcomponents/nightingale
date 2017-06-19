1d1b96373e422f0867e78dbef2b9b49015af253c
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2017-06-19 12:42:07 +0100

    cleaning up code

--- packages/protvista-sequence/src/protvista-sequence.js
+++ packages/protvista-sequence/src/protvista-sequence.js
@@ -16,8 +16,8 @@ class ProtVistaSequence extends HTMLElement {
     this._length = parseInt(this.getAttribute('length'));
     this._start = parseInt(this.getAttribute('start')) || 1;
     this._end = parseInt(this.getAttribute('end')) || this._length;
-    this._highlightStart = parseInt(this.getAttribute('highlightStart'));
-    this._highlightEnd = parseInt(this.getAttribute('highlightEnd'));
+    this._highlightstart = parseInt(this.getAttribute('highlightstart'));
+    this._highlightend = parseInt(this.getAttribute('highlightend'));
   }
 
   connectedCallback() {
@@ -36,7 +36,7 @@ class ProtVistaSequence extends HTMLElement {
   }
 
   static get observedAttributes() {return [
-    'length', 'start', 'end', 'highlightStart', 'highlightEnd'
+    'length', 'start', 'end', 'highlightstart', 'highlightend'
   ]; }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -51,7 +51,6 @@ class ProtVistaSequence extends HTMLElement {
     this._x = d3.scaleLinear()
       .range([padding.left, width - padding.right])
       .domain([this._start, this._end]);
-    const x = this._x;
 
     const svg = d3.select(this)
       .append('div')
@@ -61,42 +60,54 @@ class ProtVistaSequence extends HTMLElement {
       .attr('width', width)
       .attr('height', (height));
 
-    this.xAxis = d3.axisBottom(x);
+    this.xAxis = d3.axisBottom(this._x);
     this.axis = svg.append('g')
-      .attr('class', 'x axis')
-      .call(this.xAxis);
-    this.axis.select('.domain').remove();
-    this.axis.selectAll('.tick line').remove();
+      .attr('class', 'x axis');
+
     this.seq_g = svg.append('g')
       .attr('class', 'sequence')
-      .attr('transform', `translate(${-x(this._start-0.5)},30)`);
-    this.bases = this.seq_g.selectAll('text.base')
-      .data(this.sequence.split(''));
+      .attr('transform', `translate(0,30)`);
 
-    this.bases.enter()
-      .append('text')
-      .attr('class', 'base')
-      .style('text-anchor', 'middle')
-      .attr('x', (b,i) => x(i+1))
-      .text(d=>d);
+    this.highlighted = svg.append('rect')
+      .attr('class', 'highlighted')
+      .attr('fill', 'yellow')
+      .attr('height', height)
+
+    this._updateSequence()
   }
   _updateSequence(){
     if (this._x) {
       this._x.domain([this._start, this._end]);
+
       this.axis.call(this.xAxis);
       this.axis.select('.domain').remove();
       this.axis.selectAll('.tick line').remove();
-      this.seq_g.attr('transform',
-        `translate(${-this._x(this._start-0.5)},30)`
-      );
-      const space = this._x(2) - this._x(1)
-        - this.seq_g.select('text.base').node().getBBox().width * 0.8;
-      this.seq_g
-        .style('opacity', Math.min(1, space));
+
       this.bases = this.seq_g.selectAll('text.base')
         .data(this.sequence.split(''));
-      this.bases
-        .attr('x', (b,i) => this._x(i+1));
+      this.bases.enter()
+        .append('text')
+        .attr('class', 'base')
+        .attr('x', (b,i) => this._x(i+1))
+        .text(d=>d);
+      this.bases.attr('x', (b,i) => this._x(i+1));
+
+      const space = this._x(2) - this._x(1)
+        - this.seq_g.select('text.base').node().getBBox().width * 0.8;
+      this.seq_g.style('opacity', Math.min(1, space));
+
+      if (Number.isInteger(this._highlightstart) && Number.isInteger(this._highlightend)){
+        this.highlighted
+          .attr('x', this._x(this._highlightstart))
+          .style('opacity', 0.3)
+          .attr('width',
+            this._x(this._start +
+              Math.max(1, this._highlightend - this._highlightstart)
+            )
+          );
+      } else {
+        this.highlighted.style('opacity', 0);
+      }
     }
   }
 }
