741616f3682c1ec3cbe7a3f41d003d8255e2fb58
ljgarcia <leylajael@gmail.com>
2017-07-06 17:47:28 +0100

    Add generic adapter code.

---
+++ packages/uniprot-entry-data-adapter/src/EntryDataAdapter.js
@@ -0,0 +1,70 @@
+/*jslint node: true */
+"use strict";
+
+const loaderComponentType = 'uniprot-entry-data-loader';
+
+export class EntryDataAdapter extends HTMLElement {
+    constructor() {
+        super();
+        this._listening = false;
+        this._adaptedData = {};
+    }
+
+    connectedCallback() {
+        this._initLoaders();
+        this._addLoaderListeners();
+    }
+
+    parseEntry(data) {
+        //Specialized versions of this generic adapter should implement this method so data is actually transformed
+        // and adapted.
+        this._adaptedData = data;
+    }
+
+    get adaptedData() {
+        return this._adaptedData;
+    }
+
+    _initLoaders() {
+        let loaders = this.querySelectorAll(loaderComponentType);
+        if (loaders.length === 0) {
+            //TODO If no loaders, then at least one adapter
+            this.dispatchEvent(new CustomEvent(
+                'error',
+                {detail: 'No data to adapt as no loader was specified', bubbles: true}
+            ));
+        } else {
+            //TODO response from multiple loader should be mixed
+            this._removeChildrenInList(this, loaders, 1, loaders.length);
+        }
+    }
+
+    _removeChildrenInList(elem, list, start, end) {
+        for (let i = start; (i < end) && (i < list.length); i++) {
+            elem.removeChild(list[i]);
+        }
+    }
+
+    _addLoaderListeners() {
+        if (!this._listening) {
+            this._listening = true;
+            this.addEventListener('load', (e) => {
+                try {
+                    if (e.detail.payload.errorMessage) {
+                        throw e.detail.payload.errorMessage;
+                    }
+                    this.parseEntry(e.detail.payload);
+                    this.dispatchEvent(new CustomEvent(
+                        'ready',
+                        {detail: this._adaptedData, bubbles: true}
+                    ));
+                } catch(error) {
+                    this.dispatchEvent(new CustomEvent(
+                        'error',
+                        {detail: error, bubbles: true}
+                    ));
+                }
+            });
+        }
+    }
+}
---
+++ packages/uniprot-entry-data-adapter/src/index.js
@@ -0,0 +1,7 @@
+import {EntryDataAdapter} from './EntryDataAdapter';
+
+if (window.customElements) {
+    customElements.define('protvista-structure-adapter', EntryDataAdapter);
+}
+
+export default EntryDataAdapter;
