f607c5c1730b9bcf9cd50ec8fb32232e569f05c4
ljgarcia <leylajael@gmail.com>
2017-07-18 10:13:13 +0100

    Accept data loader or adapter as child.
    * Show and test how to use the inner adapter by using the empty-dummy-data-adapter, not really meant to be used on its own... we might remove it later...

---
+++ packages/uniprot-entry-data-adapter/src/EmptyDataAdapter.js
@@ -0,0 +1,17 @@
+/*jslint node: true */
+"use strict";
+
+import UniProtEntryDataAdapter from './UniProtEntryDataAdapter';
+
+export default class EmptyDataAdapter extends UniProtEntryDataAdapter {
+    constructor() {
+        super();
+        this._adapterType = 'empty-data-adapter';
+        this._adaptedData = {};
+    }
+
+    parseEntry(data) {
+        this._adapterType = {};
+        return this._adaptedData;
+    }
+}
--- packages/uniprot-entry-data-adapter/src/UniProtEntryDataAdapter.js
+++ packages/uniprot-entry-data-adapter/src/UniProtEntryDataAdapter.js
@@ -20,6 +20,7 @@ export default class UniProtEntryDataAdapter extends HTMLElement {
         //Specialized versions of this generic adapter should implement this method so data is actually transformed
         // and adapted.
         this._adaptedData = data;
+        return this._adaptedData;
     }
 
     get adaptedData() {
@@ -27,19 +28,14 @@ export default class UniProtEntryDataAdapter extends HTMLElement {
     }
 
     _initLoaders() {
-        let loaders = this.querySelectorAll(loaderComponentType);
-        if (loaders.length === 0) {
-            //TODO If no loaders, then at least one adapter
-            this.dispatchEvent(new CustomEvent(
-                'error',
-                {detail: 'No data to adapt as no loader was specified', bubbles: true}
-            ));
-        } else {
+        let children = this.children;
+        if (this.childElementCount !== 1) {
             this.dispatchEvent(new CustomEvent(
                 'warning',
-                {detail: 'Only one loader is allowed, the first one will be used, the others dismissed', bubbles: true}
+                {detail: 'Only one loader OR adapter is allowed, the first one will be used, the others dismissed',
+                    bubbles: true, cancelable: true}
             ));
-            this._removeChildrenInList(this, loaders, 1, loaders.length);
+            this._removeChildrenInList(this, children, 1, this.childElementCount);
         }
     }
 
@@ -70,6 +66,20 @@ export default class UniProtEntryDataAdapter extends HTMLElement {
                     this.dispatchEvent(new CustomEvent(
                         'error',
                         {detail: error, bubbles: true}
+                    ));
+                }
+            });
+            this.addEventListener('adapt', (e) => {
+                if (e.detail.adapter !== this._adapterType) {
+                    //only listen to other adapters not to itself (so infinite loop is avoided)
+                    e.stopPropagation();
+                    this.parseEntry(e.detail.data);
+                    this.dispatchEvent(new CustomEvent(
+                        'adapt',
+                        {
+                            detail: {adapter: this._adapterType, data: this._adaptedData},
+                            bubbles: true, cancelable: true
+                        }
                     ));
                 }
             });
