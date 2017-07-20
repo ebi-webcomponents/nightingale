843c03031648fb93c1bb39976a0f798177578f18
ljgarcia <leylajael@gmail.com>
2017-07-20 13:41:19 +0100

    Use big arrow.

--- packages/protvista-feature-adapter/src/ParserHelper.js
+++ packages/protvista-feature-adapter/src/ParserHelper.js
@@ -5,7 +5,7 @@ import _each from 'lodash-es/each';
 
 let _evidenceGrouping = function(ftEvidences) {
     var evidences = {};
-    _each(ftEvidences, function(ev) {
+    _each(ftEvidences, (ev) => {
         if (evidences[ev.code]) {
             evidences[ev.code].push(ev.source);
         } else {
@@ -17,12 +17,12 @@ let _evidenceGrouping = function(ftEvidences) {
 
 export default class ParserHelper {
     static groupEvidencesByCode (features) {
-        _each(features, function(ft) {
+        _each(features, (ft) => {
             if (ft.evidences) {
                 ft.evidences = _evidenceGrouping(ft.evidences);
             }
             if (ft.association) {
-                _each(ft.association, function(disease) {
+                _each(ft.association, (disease) => {
                     if (disease.evidences) {
                         disease.evidences = this._evidenceGrouping(disease.evidences);
                     }
