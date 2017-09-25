253061f10c07477a08a008b81a33c32a26227383
Xavier Watkins <xwatkins@ebi.ac.uk>
2017-09-25 16:55:23 +0100

    Remove http prefix to add https support

--- packages/interaction-viewer/src/apiLoader.js
+++ packages/interaction-viewer/src/apiLoader.js
@@ -6,75 +6,77 @@ const subcellulartreeMenu = [];
 const diseases = {};
 
 const apiLoader = {
-  load: function(accession) {
-    var promise = new Promise(function(resolve) {
-      return d3.json(`http://www.ebi.ac.uk/proteins/api/proteins/interaction/${accession}.json`, function(json) {
-        var data = apiLoader.process(json);
-        resolve(data);
-      });
-    });
-    return promise;
-  }, process: function(data) {
-    // remove interactions which are not part of current set
-    _.each(data, function(element) {
-      let interactors = [];
-      element.filterTerms = [];
+    load: function(accession) {
+        var promise = new Promise(function(resolve) {
+            return d3.json(`//www.ebi.ac.uk/proteins/api/proteins/interaction/${accession}.json`, function(json) {
+                var data = apiLoader.process(json);
+                resolve(data);
+            });
+        });
+        return promise;
+    },
+    process: function(data) {
+        // remove interactions which are not part of current set
+        _.each(data, function(element) {
+            let interactors = [];
+            element.filterTerms = [];
 
-      // Add source  to the nodes
-      _.each(element.interactions, function(interactor) {
-        // Add interaction for SELF
-        if(interactor.interactionType === 'SELF') {
-          interactor.source = element.accession;
-          interactor.id = element.accession;
-          interactors.push(interactor);
-        }
-        // TODO review this as it's not nice.
-        // TODO also save the reverse??
-        else if (_.some(data, function(d) {//Check that interactor is in the data
-            return d.accession === interactor.id;
-          })) {
-          interactor.source = element.accession;
-          interactors.push(interactor);
-        } else if (interactor.id.includes('-')) { //handle isoforms
-          // TODO handle isoforms
-          // console.log(interactor.id);
-        }
-      });
-      element.interactions = interactors;
+            // Add source  to the nodes
+            _.each(element.interactions, function(interactor) {
+                // Add interaction for SELF
+                if (interactor.interactionType === 'SELF') {
+                    interactor.source = element.accession;
+                    interactor.id = element.accession;
+                    interactors.push(interactor);
+                }
+                // TODO review this as it's not nice.
+                // TODO also save the reverse??
+                else if (_.some(data, function(d) { //Check that interactor is in the data
+                        return d.accession === interactor.id;
+                    })) {
+                    interactor.source = element.accession;
+                    interactors.push(interactor);
+                } else if (interactor.id.includes('-')) { //handle isoforms
+                    // TODO handle isoforms
+                    // console.log(interactor.id);
+                }
+            });
+            element.interactions = interactors;
 
-      if (element.subcellularLocations) {
-        for (let location of element.subcellularLocations) {
-          for (let actualLocation of location.locations) {
-            treeMenu.addStringItem(actualLocation.location.value, subcellulartreeMenu);
-            let locationSplit = actualLocation.location.value.split(', ');
-            element.filterTerms = element.filterTerms.concat(locationSplit);
-          }
-        }
-      }
-      if (element.diseases) {
-        for (let disease of element.diseases) {
-          if (disease.diseaseId) {
-            diseases[disease.diseaseId] = {
-              name: disease.diseaseId,
-              selected: false
-            };
-            element.filterTerms.push(disease.diseaseId);
-          }
-        }
-      }
-    });
-    return data;
-  }, getFilters: function(){
-    return [{
-      name: 'subcellularLocations',
-      label: 'Subcellular location',
-      type: 'tree',
-      items: subcellulartreeMenu
-    }, {
-      name: 'diseases',
-      label: 'Diseases',
-      items: _.values(diseases)
-    }];
-  }
+            if (element.subcellularLocations) {
+                for (let location of element.subcellularLocations) {
+                    for (let actualLocation of location.locations) {
+                        treeMenu.addStringItem(actualLocation.location.value, subcellulartreeMenu);
+                        let locationSplit = actualLocation.location.value.split(', ');
+                        element.filterTerms = element.filterTerms.concat(locationSplit);
+                    }
+                }
+            }
+            if (element.diseases) {
+                for (let disease of element.diseases) {
+                    if (disease.diseaseId) {
+                        diseases[disease.diseaseId] = {
+                            name: disease.diseaseId,
+                            selected: false
+                        };
+                        element.filterTerms.push(disease.diseaseId);
+                    }
+                }
+            }
+        });
+        return data;
+    },
+    getFilters: function() {
+        return [{
+            name: 'subcellularLocations',
+            label: 'Subcellular location',
+            type: 'tree',
+            items: subcellulartreeMenu
+        }, {
+            name: 'diseases',
+            label: 'Diseases',
+            items: _.values(diseases)
+        }];
+    }
 };
 module.exports = apiLoader;
--- packages/interaction-viewer/src/main.js
+++ packages/interaction-viewer/src/main.js
@@ -1,55 +1,55 @@
 (function() {
-  const interactionVis = require('./interaction-viewer');
+    const interactionVis = require('./interaction-viewer');
 
-  var loadComponent = function() {
-    class InteractionViewer extends HTMLElement {
+    var loadComponent = function() {
+        class InteractionViewer extends HTMLElement {
 
-      createdCallback() {
-        this._accession = this.getAttribute('accession');
-      }
+            createdCallback() {
+                this._accession = this.getAttribute('accession');
+            }
 
-      attachedCallback() {
-        this._render();
-      }
+            attachedCallback() {
+                this._render();
+            }
 
-      attributeChangedCallback(attrName, oldVal, newVal) {
-        if (attrName === 'accession') {
-          this._accession = newVal;
-          this._render();
+            attributeChangedCallback(attrName, oldVal, newVal) {
+                if (attrName === 'accession') {
+                    this._accession = newVal;
+                    this._render();
+                }
+            }
+
+            set accession(accession) {
+                this._accession = accession;
+            }
+
+            get accession() {
+                return this._accession;
+            }
+
+            _render() {
+                interactionVis.render({
+                    el: this,
+                    accession: this._accession
+                });
+            }
         }
-      }
 
-      set accession(accession) {
-        this._accession = accession;
-      }
-
-      get accession() {
-        return this._accession;
-      }
-
-      _render() {
-        interactionVis.render({
-          el: this,
-          accession: this._accession
-        });
-      }
+        document.registerElement('interaction-viewer', InteractionViewer);
     }
 
-    document.registerElement('interaction-viewer', InteractionViewer);
-  }
-
-// Conditional loading of polyfill
-  if ('registerElement' in document &&
-    'import' in document.createElement('link') &&
-    'content' in document.createElement('template')) {
-    loadComponent();
-  } else {
-    // polyfill the platform!
-    var e = document.createElement('script');
-    e.src = 'http://ebi-uniprot.github.io/interaction-viewer/micro.js';
-    document.body.appendChild(e);
-    document.addEventListener('WebComponentsReady', function() {
-      loadComponent();
-    });
-  }
+    // Conditional loading of polyfill
+    if ('registerElement' in document &&
+        'import' in document.createElement('link') &&
+        'content' in document.createElement('template')) {
+        loadComponent();
+    } else {
+        // polyfill the platform!
+        var e = document.createElement('script');
+        e.src = '//ebi-uniprot.github.io/interaction-viewer/micro.js';
+        document.body.appendChild(e);
+        document.addEventListener('WebComponentsReady', function() {
+            loadComponent();
+        });
+    }
 })()
