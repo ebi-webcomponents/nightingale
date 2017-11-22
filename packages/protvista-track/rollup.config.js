58bd9b8bb05ba2018b2393c272e3a599ccb8f5ab
ljgc <leylajael@gmail.com>
2017-11-22 16:35:51 +0000

    Update webcomponent imports.
    Still with bugs...

--- packages/protvista-track/rollup.config.js
+++ packages/protvista-track/rollup.config.js
@@ -2,11 +2,13 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-  entry: 'src/index.js',
-  format: 'iife',
-  moduleName: 'ProtVistaTrack',
-  dest: 'dist/protvista-track.js',
-  sourceMap: true,
+  input: 'src/index.js',
+  name: 'ProtVistaTrack',
+  sourcemap: true,
+  output: {
+      file: 'dist/protvista-track.js',
+      format: 'iife',
+  },
   external: ['d3'],
   // paths: {
   //   d3: 'https://d3js.org/d3.v4.min.js'
--- packages/protvista-track/src/index.js
+++ packages/protvista-track/src/index.js
@@ -1,9 +1,18 @@
 import ProtVistaTrack from './protvista-track';
 import DataLoader from 'data-loader';
 
+const loadComponent = function() {
+    customElements.define('protvista-track', ProtVistaTrack);
+    customElements.define('protvista-config-data-loader', DataLoader);
+};
+
+// Conditional loading of polyfill
 if (window.customElements) {
-  customElements.define('protvista-track', ProtVistaTrack);
-  customElements.define('protvista-config-data-loader', DataLoader);
+    loadComponent();
+} else {
+    document.addEventListener('WebComponentsReady', function() {
+        loadComponent();
+    });
 }
 
 export default ProtVistaTrack;
