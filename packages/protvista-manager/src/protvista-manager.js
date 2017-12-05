d0be82cadd67b02afe8d7bfe2e3e6f61d964ea6b
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2017-12-05 10:31:13 +0000

    propagating changes of more than one attribute at the time

--- packages/protvista-manager/src/protvista-manager.js
+++ packages/protvista-manager/src/protvista-manager.js
@@ -4,6 +4,10 @@ class ProtVistaManager extends HTMLElement {
   constructor() {
     super();
     this._attributes = this.getAttribute('attributes').split(' ');
+    if (this._attributes.indexOf('type') !== -1)
+      throw new Error("'type' can't be used as a [rot vista attribute]")
+    if (this._attributes.indexOf('value') !== -1)
+      throw new Error("'value' can't be used as a [rot vista attribute]")
   }
 
   connectedCallback() {
@@ -12,6 +16,13 @@ class ProtVistaManager extends HTMLElement {
         if (this._attributes.indexOf(e.detail.type) !== -1){
           for (const ch of this.children){
             ch.setAttribute(e.detail.type, e.detail.value);
+          }
+        }
+        for (let key in e.detail) {
+          if (this._attributes.indexOf(key) !== -1){
+            for (const ch of this.children){
+              ch.setAttribute(key, e.detail[key]);
+            }
           }
         }
       });
