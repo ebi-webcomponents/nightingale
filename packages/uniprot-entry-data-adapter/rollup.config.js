953489bedf0c4baf3535a23a3a781f73bdf10f14
ljgc <leylajael@gmail.com>
2017-12-12 14:31:32 +0000

    Merge remote-tracking branch 'origin/master'

--- packages/uniprot-entry-data-adapter/rollup.config.js
+++ packages/uniprot-entry-data-adapter/rollup.config.js
@@ -3,10 +3,12 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
     entry: 'src/index.js',
-    dest: 'dist/UniProtEntryDataAdapter.js',
-    format: 'iife',
-    moduleName: 'UniProtEntryDataAdapter',
+    name: 'UniProtEntryDataAdapter',
     sourceMap: true,
+    output: {
+        file: 'dist/UniProtEntryDataAdapter.js',
+        format: 'iife',
+    },
     plugins: [
         nodeResolve({jsnext: true}),
         babel({
