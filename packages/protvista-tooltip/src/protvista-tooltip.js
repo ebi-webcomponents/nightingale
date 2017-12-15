84679d8aea3441fe5edd6b0471039870cf1c6bcd
ljgc <leylajael@gmail.com>
2017-12-15 15:51:45 +0000

    Add container and (empty by now) table.

--- packages/protvista-tooltip/src/protvista-tooltip.js
+++ packages/protvista-tooltip/src/protvista-tooltip.js
@@ -1,22 +1,80 @@
+/* eslint-disable no-console */
 class ProtvistaTooltip extends HTMLElement {
     constructor() {
         super();
+        this._left = 0;
+        this._top = 0;
+
         // get properties here
-        const shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.innerHTML = `
-            <style>
-            :host {
-                // Style your component
-            }
-            </style>
-            `;
+        this._shadowRoot = this.attachShadow({mode: 'open'});
+        this._shadowRoot.innerHTML = '';
+    }
+
+    static get observedAttributes() {
+        return [
+            'left', 'top' //relative to parent
+        ];
     }
 
     connectedCallback() {
-        console.log('callback');
+        if (this._data)
+            this._createTooltip();
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {}
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue){
+            const intValue = parseFloat(newValue);
+            this[`_${name}`] = isNaN(intValue) ? oldValue : intValue;
+            this._updateTooltip();
+        }
+    }
+
+    set data(data) {
+        this._data = data;
+        this._createTooltip();
+    }
+
+    _createTooltip() {
+        this._shadowRoot.innerHTML = '';
+        const container = this._createContainer();
+        const ttTable = this._populateTooltip();
+        container.appendChild(ttTable);
+        this._shadowRoot.appendChild(container);
+    }
+
+    _createContainer() {
+        const container = document.createElement('div');
+        container.className = 'tooltip-container';
+
+        const closeSpan = document.createElement('span');
+        container.appendChild(closeSpan);
+        closeSpan.innerText = 'X';
+        closeSpan.className = 'tooltip-close';
+        closeSpan.addEventListener('click', () => {
+            container.style = 'transition: 20; opacity: 0; display: none';
+            this._shadowRoot.innerHTML = '';
+        });
+
+        return container;
+    }
+
+    _populateTooltip() {
+        const ttTable = document.createElement('table');
+        const ttRow = document.createElement('tr');
+        const ttHeader = document.createElement('th');
+        ttHeader.colSpan = 2;
+        ttHeader.innerText = this._data.title;
+        ttRow.appendChild(ttHeader);
+        ttTable.appendChild(ttRow);
+        for (let elem of this._data.elements) {
+            console.log(elem);
+        }
+        return ttTable;
+    }
+
+    _updateTooltip() {
+
+    }
 }
 
 export default ProtvistaTooltip;
