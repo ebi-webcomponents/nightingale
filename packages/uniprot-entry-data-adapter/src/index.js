a3f6d9afafeb5e9cbc02bdea4cb43fdfd6402938
ljgc <leylajael@gmail.com>
2017-12-20 10:04:36 +0000

    Remove empty dummy adapter.
    And use the same uniprot-entry-data-adapter to show how it works with nested adapters.

--- packages/uniprot-entry-data-adapter/src/index.js
+++ packages/uniprot-entry-data-adapter/src/index.js
@@ -1,9 +1,7 @@
 import UniProtEntryDataAdapter from './UniProtEntryDataAdapter';
-import EmptyDataAdapter from './EmptyDataAdapter';
 
 if (window.customElements) {
     customElements.define('uniprot-entry-data-adapter', UniProtEntryDataAdapter);
-    customElements.define('empty-dummy-data-adapter', EmptyDataAdapter);
 }
 
 export default UniProtEntryDataAdapter;
