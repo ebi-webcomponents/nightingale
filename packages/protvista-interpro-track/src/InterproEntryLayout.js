24ba14fac1a1800b334da5996b612a5a51c3b0d7
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-02-01 15:31:45 +0000

    adding residues to contributors

--- packages/protvista-interpro-track/src/InterproEntryLayout.js
+++ packages/protvista-interpro-track/src/InterproEntryLayout.js
@@ -16,18 +16,51 @@ export default class InterproEntryLayout extends DefaultLayout{
       this.yPos[feature.accession] = this._padding;
       yPos = this.height[feature.accession] + 2*this._padding;
       this.maxYPos = Math.max(this.maxYPos, yPos);
+      if (feature.residues){
+        for (let i=0; i<feature.residues.length; i++) {
+          const resGroup = feature.residues[i];
+          for (let j=0; j<resGroup.locations.length; j++) {
+            this.height[`${resGroup.accession}_${i}_${j}`] = this.expanded ? CHILD_HEIGHT: 0;
+            this.yPos[`${resGroup.accession}_${i}_${j}`] = this.maxYPos + this._padding;
+            yPos += 2*this._padding + this.height[`${resGroup.accession}_${i}_${j}`];
+            this.maxYPos = Math.max(this.maxYPos, yPos)
+          }
+        }
+      }
     }
     if (children) for (let child of children) {
       this.height[child.accession] = this.expanded ? CHILD_HEIGHT: 0;
       this.yPos[child.accession] = this.maxYPos + this._padding;
       yPos += 2*this._padding + this.height[child.accession];
-      this.maxYPos = Math.max(this.maxYPos, yPos)
+      this.maxYPos = Math.max(this.maxYPos, yPos);
+      if (child.residues){
+        for (let i=0; i<child.residues.length; i++) {
+          const resGroup = child.residues[i];
+          for (let j=0; j<resGroup.locations.length; j++) {
+            this.height[`${resGroup.accession}_${i}_${j}`] = this.expanded && child.expanded ? CHILD_HEIGHT: 0;
+            this.yPos[`${resGroup.accession}_${i}_${j}`] = this.maxYPos + this._padding;
+            yPos += 2*this._padding + this.height[`${resGroup.accession}_${i}_${j}`];
+            this.maxYPos = Math.max(this.maxYPos, yPos)
+          }
+        }
+      }
     }
   }
+  _getAccFromFeature(feature){
+    let acc = '';
+    if (typeof feature === 'string') {
+      acc = feature;
+    } else if (feature.accession){
+        acc = feature.accession;
+    }
+    return acc;
+  }
   getFeatureYPos(feature) {
-    return feature.accession in this.yPos ? this.yPos[feature.accession] : 0;
+    const acc = this._getAccFromFeature(feature);
+    return acc in this.yPos ? this.yPos[acc] : 0;
   }
   getFeatureHeight(feature) {
-    return feature.accession in this.height ? this.height[feature.accession] : 0;
+    const acc = this._getAccFromFeature(feature);
+    return acc in this.height ? this.height[acc] : 0;
   };
 }
--- packages/protvista-interpro-track/src/protvista-interpro-track.js
+++ packages/protvista-interpro-track/src/protvista-interpro-track.js
@@ -12,6 +12,8 @@ const height = 44,
 
 class ProtVistaInterproTrack extends ProtVistaTrack {
   _createTrack() {
+    console.log("Features!!!");
+
     this._layoutObj.expanded = this._expanded;
     super._createTrack();
     this.children_g = null;
@@ -21,13 +23,17 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
     this._expanded = this.hasAttribute('expanded');
     this._haveCreatedFeatures = false;
   }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name==='expanded' && oldValue !== newValue && this._contributors){
+      for (let c of this._contributors){
+        c.expanded = !oldValue;
+      }
+    }
+    super.attributeChangedCallback(name, oldValue, newValue);
+  }
 
   set data(data) {
     this._data = this.normalizeLocations(data);
-    // for (let feature of this._data) {
-    //   feature.children = this.normalizeLocations(feature.children || []);
-    //   feature.children.forEach(child=>child.parent = feature);
-    // }
     this._createTrack();
   }
   set contributors(contributors) {
@@ -50,7 +56,58 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
       this.refresh();
     }
   }
+  _createResidueGroup(baseG){
+    return baseG.selectAll('g.residues-group')
+      .data(d => d.residues ? d.residues.map(r => Object.assign({}, r, {feature: d})) : [])
+        .enter().append('g')
+          .attr('class', 'residues-group')
+          .selectAll('g.residues-locations')
+          .data((d, i) =>
+            d.locations.map((loc) =>
+             Object.assign({}, loc, {
+               accession: d.accession,
+               feature: d.feature,
+               location: loc,
+               i:i,
+               // expended: true
+             }))
+          )
+          .enter().append('g')
+            .attr('class', 'residues-locations');
+  }
+  _createResiduePaths(baseG){
+    return baseG
+      .selectAll('g.residue')
+      .data((d, j) => d.fragments.map((loc) => Object.assign({}, loc, {accession: d.accession, feature: d.feature, location: d.location, i: d.i, j:j})))
+      .enter()
+      .append('path')
+        .attr('class', 'feature')
+        .attr('d', (f,j) =>
+          this._featureShape.getFeatureShape(
+            this.xScale(2) - this.xScale(1), this._layoutObj.getFeatureHeight(`${f.accession}_${f.i}_${f.j}`),
+              f.end ? f.end - f.start + 1 : 1, 'rectangle'
+          )
+        )
+        .attr('transform',
+          f =>'translate(' + this.xScale(f.start)+ ',' + (padding.top + this._layoutObj.getFeatureYPos(`${f.accession}_${f.i}_${f.j}`)) + ')'
+        )
+        .attr('fill', f => this._getFeatureColor(f))
+        .attr('stroke', 'transparent');
+  }
+  _refreshResiduePaths(baseG){
+    baseG
+      .attr('d', f =>
+        this._featureShape.getFeatureShape(
+          this.xScale(2) - this.xScale(1), this._layoutObj.getFeatureHeight(`${f.accession}_${f.i}_${f.j}`),
+            f.end ? f.end - f.start + 1 : 1, 'rectangle'
+        )
+      )
+      .attr('transform',
+        f =>'translate(' + this.xScale(f.start)+ ',' + (padding.top + this._layoutObj.getFeatureYPos(`${f.accession}_${f.i}_${f.j}`)) + ')'
+      )
+      .attr('fill', f => this._getFeatureColor(f));
 
+  }
   _createFeatures(){
     this._layoutObj.init(this._data, this._contributors);
     this.featuresG = this.seq_g.selectAll('g.feature-group')
@@ -66,9 +123,8 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
           .attr('class', 'location-group');
 
     this.features = this.locations
-      .selectAll('g.fragment-group')
+      .selectAll('path.feature')
       .data(d => d.fragments.map((loc) => Object.assign({}, loc, {feature: d.feature})))
-      // .data(d=>d.fragments.map(({...l})=>({feature:d.feature, ...l})))
       .enter()
       .append('path')
         .attr('class', 'feature')
@@ -108,6 +164,8 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
             detail: f, bubbles:true, cancelable: true
           }));
         });
+    this.residues_g = this._createResidueGroup(this.featuresG);
+    this.residues_loc = this._createResiduePaths(this.residues_g);
 
 
     if (this._contributors) {
@@ -118,52 +176,60 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
       this.childrenGroup = this.children_g.append('g')
         .attr('class', 'children-group')
         .attr('visibility', this._expanded ? 'visible' : 'hidden' );
-      this.featureChildren = this.childrenGroup.selectAll('g.child-group')
+      this.childGroup = this.childrenGroup.selectAll('g.child-group')
         .data(d => this._contributors)
         .enter()
         .append('g')
-          .attr('class', 'child-group')
-          .selectAll('g.child-location-group')
-            .data(d => d.locations.map((loc) => Object.assign({}, loc, {feature: d})))
-            .enter().append('g')
-              .attr('class', 'child-location-group')
-              .selectAll('path.child-fragment')
-              .data(d => d.fragments.map((loc) => Object.assign({}, loc, {feature: d.feature})))
-              .enter()
-              .append('path')
-                .attr('class', 'child-fragment')
-                .attr('d', f =>
-                  this._featureShape.getFeatureShape(
-                    this.xScale(2) - this.xScale(1), this._layoutObj.getFeatureHeight(f.feature),
-                      f.end ? f.end - f.start + 1: 1, this._getShape(f.feature)
-                  )
-                )
-                .attr('transform', f =>
-                  'translate(' + this.xScale(f.start)+ ',' + (padding.top + this._layoutObj.getFeatureYPos(f.feature)) + ')'
-                )
-                .attr('fill', f => this._getFeatureColor(f.feature))
-                .attr('stroke', f => this._getFeatureColor(f.feature))
-                .on('click', (f, i, d) => {
-                  this.dispatchEvent(new CustomEvent("entryclick", {
-                    detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
-                  }));
-                })
-                .on('mouseover', (f, i, d) => {
-                  this.dispatchEvent(new CustomEvent("change", {
-                    detail: {highlightend: f.end, highlightstart: f.start}, bubbles:true, cancelable: true
-                  }));
-                  this.dispatchEvent(new CustomEvent("entrymouseover", {
-                    detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
-                  }));
-                })
-                .on('mouseout', (f, i, d) => {
-                  this.dispatchEvent(new CustomEvent("change", {
-                    detail: {highlightend: null, highlightstart: null}, bubbles:true, cancelable: true
-                  }));
-                  this.dispatchEvent(new CustomEvent("entrymouseout", {
-                    detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
-                  }));
-                });
+          .attr('class', 'child-group');
+      this.featureChildren = this.childGroup.selectAll('g.child-location-group')
+        .data(d => {
+          d.expanded=this._expanded;
+          return d.locations.map((loc) => Object.assign({}, loc, {feature: d}))
+        })
+        .enter().append('g')
+          .attr('class', 'child-location-group')
+          .selectAll('path.child-fragment')
+          .data(d => d.fragments.map((fragment) => Object.assign({}, fragment, {feature: d.feature})))
+          .enter()
+          .append('path')
+            .attr('class', 'child-fragment')
+            .attr('d', f =>
+              this._featureShape.getFeatureShape(
+                this.xScale(2) - this.xScale(1), this._layoutObj.getFeatureHeight(f.feature),
+                  f.end ? f.end - f.start + 1: 1, this._getShape(f.feature)
+              )
+            )
+            .attr('transform', f =>
+              'translate(' + this.xScale(f.start)+ ',' + (padding.top + this._layoutObj.getFeatureYPos(f.feature)) + ')'
+            )
+            .attr('fill', f => this._getFeatureColor(f.feature))
+            .attr('stroke', f => this._getFeatureColor(f.feature))
+            .on('click', (f, i, d) => {
+              f.feature.expanded = !f.feature.expanded;
+              this.refresh();
+              this.dispatchEvent(new CustomEvent("entryclick", {
+                detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
+              }));
+            })
+            .on('mouseover', (f, i, d) => {
+              this.dispatchEvent(new CustomEvent("change", {
+                detail: {highlightend: f.end, highlightstart: f.start}, bubbles:true, cancelable: true
+              }));
+              this.dispatchEvent(new CustomEvent("entrymouseover", {
+                detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
+              }));
+            })
+            .on('mouseout', (f, i, d) => {
+              this.dispatchEvent(new CustomEvent("change", {
+                detail: {highlightend: null, highlightstart: null}, bubbles:true, cancelable: true
+              }));
+              this.dispatchEvent(new CustomEvent("entrymouseout", {
+                detail: Object.assign(f, {target: d[i]}), bubbles:true, cancelable: true
+              }));
+            });
+        this.child_residues_g = this._createResidueGroup(this.childGroup);
+        this.child_residues_loc = this._createResiduePaths(this.child_residues_g);
+
     }
     this.svg.attr("height", this._layoutObj.maxYPos);
     this._haveCreatedFeatures = true;
@@ -185,6 +251,10 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
         .attr('transform', f =>
           'translate(' + this.xScale(f.start)+ ',' + (padding.top + this._layoutObj.getFeatureYPos(f.feature)) + ')'
         );
+
+      this.residues_g.attr('visibility', this._expanded ? 'visible' : 'hidden' );
+      this._refreshResiduePaths(this.residues_loc);
+
       if (this._contributors) {
         this.childrenGroup.attr('visibility', this._expanded ? 'visible' : 'hidden' );
         this.featureChildren
@@ -199,6 +269,9 @@ class ProtVistaInterproTrack extends ProtVistaTrack {
           )
           .attr('fill', f => this._getFeatureColor(f.feature))
           .attr('stroke', f => this._getFeatureColor(f.feature));
+        this.child_residues_g.attr('visibility', d => d.feature.expanded ? 'visible' : 'hidden');
+        this._refreshResiduePaths(this.child_residues_loc);
+
       }
       this._updateHighlight();
       this.svg.attr("height", this._layoutObj.maxYPos);
