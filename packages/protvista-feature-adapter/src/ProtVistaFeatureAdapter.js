d1334c8af299ebf8d02c01a0281d1690a121779d
Xavier Watkins <xavier.watkins@gmail.com>
2018-02-13 15:54:43 +0000

    Remove uniprot-entry-data-loader

--- packages/protvista-feature-adapter/src/ProtVistaFeatureAdapter.js
+++ packages/protvista-feature-adapter/src/ProtVistaFeatureAdapter.js
@@ -1,18 +1,12 @@
-/*jslint node: true */
-"use strict";
-
 import UniProtEntryDataAdapter from 'uniprot-entry-data-adapter';
 import ParserHelper from './ParserHelper';
 
 export default class ProtVistaFeatureAdapter extends UniProtEntryDataAdapter {
     constructor() {
         super();
-        this._adapterType = 'protvista-feature-adapter';
-        this._adaptedData = {};
     }
 
     parseEntry(data) {
-        console.log('parsing');
         this._adaptedData = data.features;
 
         if (this._adaptedData && (this._adaptedData.length !== 0)) {
