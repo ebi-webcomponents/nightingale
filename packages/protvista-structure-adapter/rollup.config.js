1083ded9425a363d7b263b2c2dae3d6e5d7b815d
ljgarcia <ljgarcia@ebi.ac.uk>
2018-05-10 14:45:39 +0100

    Update config files.

--- packages/protvista-structure-adapter/rollup.config.js
+++ packages/protvista-structure-adapter/rollup.config.js
@@ -2,15 +2,18 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-    entry: 'src/index.js',
-    dest: 'dist/ProtVistaStructureAdapter.js',
-    format: 'iife',
-    moduleName: 'ProtVistaStructureAdapter',
-    sourceMap: true,
+    input: 'src/index.js',
+    name: 'ProtVistaStructureAdapter',
+    sourcemap: true,
+    output: {
+        file: 'dist/ProtVistaStructureAdapter.js',
+        format: 'iife',
+    },
+    external: ['uniprot-entry-data-adapter'],
     plugins: [
         nodeResolve({jsnext: true}),
         babel({
             exclude: 'node_modules/**'
         })
-    ],
+    ]
 };
