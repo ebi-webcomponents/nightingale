99cea1b242b801af956b8869adebaf8be2bd4ef5
ljgarcia <ljgarcia@ebi.ac.uk>
2018-05-17 12:07:53 +0100

    Return coverage rather than 3d structures as features.

--- packages/protvista-structure-adapter/src/StructureDataParser.js
+++ packages/protvista-structure-adapter/src/StructureDataParser.js
@@ -3,6 +3,8 @@
 
 import ldFilter from 'lodash-es/filter';
 import ldMap from 'lodash-es/map';
+import ldForEach from 'lodash-es/foreach';
+import ldRemove from 'lodash-es/remove';
 import ParserHelper from './ParserHelper';
 
 const featureType = 'PDBE_COV';
@@ -38,26 +40,67 @@ export default class StructureDataParser {
         }
     }
 
-    _parseValidEntry(data) {
-        this._pdbFeatures = [];
+    _getAllFeatureStructures(data) {
+        let allFeatureStructures = [];
         const structures = ldFilter(data.dbReferences, (reference) => {
             return reference.type === 'PDB';
         });
-        this._pdbFeatures = ldMap(structures, (structure) => {
+        allFeatureStructures = ldMap(structures, (structure) => {
             const beginEnd = structure.properties.chains
                 ? ParserHelper.getBeginEnd(structure.properties.chains) : {start: 0, end: 0};
             return {
                 type: featureType,
                 category: featureCategory,
-                description: ParserHelper.getDescription(structure.properties),
+                structures: [
+                    {
+                        description: ParserHelper.getDescription(structure.properties),
+                        start: beginEnd.begin,
+                        end: beginEnd.end,
+                        xrefs: [{
+                            name: 'PDB',
+                            id: structure.id,
+                            url: 'http://www.ebi.ac.uk/pdbe-srv/view/entry/' + structure.id
+                        }]
+                    }
+                ],
                 start: beginEnd.begin,
-                end: beginEnd.end,
-                xrefs: [{
-                    name: 'PDB',
-                    id: structure.id,
-                    url: 'http://www.ebi.ac.uk/pdbe-srv/view/entry/' + structure.id
-                }]
+                end: beginEnd.end
             };
+        });
+        return allFeatureStructures;
+    }
+
+    _getOverlapping(ftStructure, startEnd) {
+        const overlapping = ldRemove(this._pdbFeatures, (ftCoverage) => {
+            if (((ftCoverage.start <= ftStructure.start) && (ftStructure.start <= ftCoverage.end))
+                || ((ftCoverage.start <= ftStructure.end) && (ftStructure.end <= ftCoverage.end))
+                || ((ftStructure.start <= ftCoverage.end) && (ftCoverage.end <= ftStructure.end))) {
+                startEnd.minStart = Math.min(startEnd.minStart, ftStructure.start, ftCoverage.start);
+                startEnd.maxEnd = Math.max(startEnd.maxEnd, ftStructure.end, ftCoverage.end);
+                return true;
+            }
+            return false;
+        });
+        return overlapping;
+    }
+
+    _parseValidEntry(data) {
+        this._pdbFeatures = [];
+        const allFeatureStructures = this._getAllFeatureStructures(data);
+
+        ldForEach(allFeatureStructures, (ftStructure) => {
+            const startEnd = {minStart: Number.MAX_SAFE_INTEGER, maxEnd: 1};
+            const overlapping = this._getOverlapping(ftStructure, startEnd);
+            if (overlapping.length === 0) {
+                this._pdbFeatures.push(ftStructure);
+            } else {
+                ftStructure.start = startEnd.minStart;
+                ftStructure.end = startEnd.maxEnd;
+                ldForEach(overlapping, (overlap) => {
+                    ftStructure.structures = ftStructure.structures.concat(overlap.structures);
+                });
+                this._pdbFeatures.push(ftStructure);
+            }
         });
     }
 }
