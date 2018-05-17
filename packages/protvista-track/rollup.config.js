4b5086bb609161ccce21140f9ec17cf05cb3ef87
ljgarcia <ljgarcia@ebi.ac.uk>
2018-05-17 13:46:03 +0100

    Update rolloup config.

--- packages/protvista-track/rollup.config.js
+++ packages/protvista-track/rollup.config.js
@@ -3,11 +3,15 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  name: 'ProtVistaTrack',
-  sourcemap: true,
   output: {
       file: 'dist/protvista-track.js',
       format: 'iife',
+      sourcemap: true,
+      name: 'ProtVistaTrack',
+      globals: {
+        'd3': 'd3',
+        'provista-zoomable': 'ProtvistaZoomable'
+      }
   },
   external: ['d3', 'protvista-zoomable'],
   // paths: {
