23c84db5f3ce2aa25d7cafb950def7add550d03e
ljgarcia <ljgarcia@ebi.ac.uk>
2018-07-06 10:23:51 +0100

    Add tooltip and start/end.

--- packages/protvista-topology-adapter/src/ProtVistaTopologyAdapter.js
+++ packages/protvista-topology-adapter/src/ProtVistaTopologyAdapter.js
@@ -14,8 +14,8 @@ export default class ProtVistaTopologyAdapter extends ProtVistaUniProtEntryAdapt
         this._adaptedData = [];
         if (data && (data.length !== 0)) {
             this._adaptedData = this._parser.parseEntry(data);
-        //    this._adaptedData.map(feature => feature.tooltipContent = this._basicHelper.formatTooltip(feature));
-        //    this._adaptedData = this._basicHelper.renameProperties(this._adaptedData);
+            this._adaptedData.map(feature => feature.tooltipContent = this._basicHelper.formatTooltip(feature));
+            this._adaptedData = this._basicHelper.renameProperties(this._adaptedData);
         }
         return this._adaptedData;
     }
