bae6035366ec4d0a3f5c624d847a679f9d419d4d
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-07-20 17:13:08 +0100

    adjusting coordinates

--- packages/protvista-sequence/src/protvista-sequence.js
+++ packages/protvista-sequence/src/protvista-sequence.js
@@ -43,12 +43,15 @@ class ProtVistaSequence extends ProtvistaZoomable {
       .attr('width', this.width)
       .attr('height', (height));
 
+    this.seq_bg = super.svg.append('g')
+      .attr('class', 'background');
+
     this.axis = super.svg.append('g')
       .attr('class', 'x axis');
 
     this.seq_g = super.svg.append('g')
       .attr('class', 'sequence')
-      .attr('transform', `translate(0,30)`);
+      .attr('transform', `translate(0,${0.75*height})`);
 
     this.highlighted = super.svg.append('rect')
       .attr('class', 'highlighted')
@@ -60,8 +63,13 @@ class ProtVistaSequence extends ProtvistaZoomable {
 
   refresh(){
     if (this.axis) {
-      this.xAxis = axisBottom(super.xScale);
+      const ftWidth = this.xScale(2)-this.xScale(1);
+      const half = ftWidth/2;
+      this.xAxis = axisBottom(this.xScale)
+        .tickFormat(d => Number.isInteger(d) ? d : '');
       this.axis.call(this.xAxis);
+
+      this.axis.attr("transform", `translate(${half},0)`)
       this.axis.select('.domain').remove();
       this.axis.selectAll('.tick line').remove();
 
@@ -70,20 +78,35 @@ class ProtVistaSequence extends ProtvistaZoomable {
       this.bases.enter()
         .append('text')
         .attr('class', 'base')
-        .attr('x', (b,i) => super.xScale(i+1))
+        .attr('text-anchor', 'middle')
+        .attr('x', (b,i) => (this.xScale(i+1)+half))
         .text(d=>d)
         .attr('style','font-family:monospace');
-      this.bases.attr('x', (b,i) => super.xScale(i+1));
+      this.bases.attr('x', (b,i) => (this.xScale(i+1)+half));
+
+      this.background = this.seq_bg.selectAll('rect.base_bg')
+        .data(this.sequence.split(''));
+      this.background.enter()
+        .append('rect')
+        .attr('class', 'base_bg')
+        .attr('fill', (d,i) => i%2 ? '#ccc': '#eee')
+        .attr('height', height)
+        .attr('width', ftWidth)
+        .attr('x', (b,i) => this.xScale(i+1));
+      this.background
+        .attr('width', ftWidth)
+        .attr('x', (b,i) => this.xScale(i+1));
 
       const space = super.xScale(2) - super.xScale(1)
         - this.seq_g.select('text.base').node().getBBox().width * 0.8;
       this.seq_g.style('opacity', Math.min(1, space));
+      this.background.style('opacity', Math.min(1, space));
 
       if (Number.isInteger(this._highlightstart) && Number.isInteger(this._highlightend)){
         this.highlighted
-          .attr('x', super.xScale(this._highlightstart - 0.5))
+          .attr('x', super.xScale(this._highlightstart))
           .style('opacity', 0.3)
-          .attr('width', (super.xScale(2) - super.xScale(1))*
+          .attr('width', ftWidth*
             (this._highlightend - this._highlightstart + 1)
           );
       } else {
