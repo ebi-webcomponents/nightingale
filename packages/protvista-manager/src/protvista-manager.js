c00d9a1439ce67813c707d920077de27edd3ce4f
Xavier Watkins <xavier.watkins@gmail.com>
2018-08-09 17:06:13 +0100

    Adding support for boolean attributes

--- packages/protvista-manager/src/protvista-manager.js
+++ packages/protvista-manager/src/protvista-manager.js
@@ -1,4 +1,3 @@
-
 class ProtVistaManager extends HTMLElement {
 
   constructor() {
@@ -7,23 +6,33 @@ class ProtVistaManager extends HTMLElement {
   }
 
   static get observedAttributes() {
-      return ['attributes'];
+    return ['attributes'];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue !== newValue) {
-        if (name === "attributes"){
+    if (oldValue !== newValue) {
+      if (name === "attributes") {
         this._attributes = newValue.split(' ');
         if (this._attributes.indexOf('type') !== -1)
           throw new Error("'type' can't be used as a protvista attribute")
         if (this._attributes.indexOf('value') !== -1)
           throw new Error("'value' can't be used as a protvista attribute")
-        }
       }
+    }
   }
 
-  _registerProtvistaDescendents(element){
-    for (const child of element.children){
+  _setAttributes(elements, type, value) {
+    for (const el of elements) {
+      if (value === false) {
+        el.removeAttribute(type);
+      } else {
+        el.setAttribute(type, typeof (value) === 'boolean' ? '' : value);
+      }
+    }
+  }
+
+  _registerProtvistaDescendents(element) {
+    for (const child of element.children) {
       if (child.isManaged)
         this.protvistaElements.add(child);
       this._registerProtvistaDescendents(child);
@@ -31,18 +40,14 @@ class ProtVistaManager extends HTMLElement {
   }
 
   addListeners() {
-    for (const child of this.protvistaElements){
+    for (const child of this.protvistaElements) {
       child.addEventListener("change", e => {
-        if (this._attributes.indexOf(e.detail.type) !== -1){
-          for (const ch of this.protvistaElements){
-            ch.setAttribute(e.detail.type, e.detail.value);
-          }
+        if (this._attributes.indexOf(e.detail.type) !== -1) {
+          this._setAttributes(this.protvistaElements, e.detail.type, e.detail.value);
         }
         for (let key in e.detail) {
-          if (this._attributes.indexOf(key) !== -1){
-            for (const ch of this.protvistaElements){
-              ch.setAttribute(key, e.detail[key]);
-            }
+          if (this._attributes.indexOf(key) !== -1) {
+            this._setAttributes(this.protvistaElements, key, e.detail[key]);
           }
         }
       });
@@ -54,13 +59,13 @@ class ProtVistaManager extends HTMLElement {
     this.addListeners();
     var that = this;
 
-    if('MutationObserver' in window) {
+    if ('MutationObserver' in window) {
       this.mutationObserver = new MutationObserver(mutationList => {
         that._registerProtvistaDescendents(that);
         that.addListeners();
       });
       this.mutationObserver.observe(this, {
-        childList:true
+        childList: true
       });
     }
   }
