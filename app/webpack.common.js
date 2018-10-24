dca8ce3551b6591b8e694c346e67aa2d9830efed
Xavier Watkins <xavier.watkins@gmail.com>
2018-10-24 10:32:06 +0100

    Update build config and add CI

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -3,10 +3,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-console.log(__dirname);
-
 module.exports = {
-  mode: "development",
   name: "site",
   entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.jsx")],
   resolve: {
@@ -16,7 +13,6 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, "src")],
         use: {
           loader: "babel-loader",
           options: {
@@ -54,12 +50,5 @@ module.exports = {
       template: `${__dirname}/index.html`,
       filename: "index.html"
     })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "build"),
-    compress: true,
-    host: "localhost",
-    port: 39093,
-    historyApiFallback: true
-  }
+  ]
 };
---
+++ app/webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "cheap-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    host: "localhost",
+    port: 39093,
+    historyApiFallback: true
+  }
+});
