19453eb8e3146be4e8a8624b15694e42bf0e3f41
Xavier Watkins <xavier.watkins@gmail.com>
2018-10-30 13:35:33 +0000

    Add tracks to manager example

--- app/src/components/ProtvistaManager.jsx
+++ app/src/components/ProtvistaManager.jsx
@@ -6,12 +6,18 @@ import ProtvistaTrack from "protvista-track";
 import ProtvistaNavigation from "protvista-navigation";
 import ProtvistaSequence from "protvista-sequence";
 import ProtvistaVariation from "protvista-variation";
+import ProtvistaInterproTrack from "protvista-interpro-track";
 import loadWebComponent from "../utils/load-web-component";
 import variantData from "../mocks/variants.json";
+import sequence from "../mocks/sequence.json";
+import { dataIPR, signatures } from "../mocks/interpro";
 
 class ProtvistaManagerWrapper extends Component {
   componentDidMount() {
     document.querySelector("#variation-track").data = variantData;
+    document.querySelector("#interpro-track").data = dataIPR;
+    document.querySelector("#interpro-track").contributors = signatures;
+    document.querySelector("#sequence-track").data = sequence;
   }
 
   render() {
@@ -20,6 +26,7 @@ class ProtvistaManagerWrapper extends Component {
     loadWebComponent("protvista-track", ProtvistaTrack);
     loadWebComponent("protvista-navigation", ProtvistaNavigation);
     loadWebComponent("protvista-sequence", ProtvistaSequence);
+    loadWebComponent("protvista-interpro-track", ProtvistaInterproTrack);
     loadWebComponent("protvista-variation", ProtvistaVariation);
     loadWebComponent("data-loader", DataLoader);
     return (
@@ -28,56 +35,29 @@ class ProtvistaManagerWrapper extends Component {
           attributes="length displaystart displayend highlightstart highlightend variantfilters"
           id="example"
         >
-          <protvista-navigation
-            length="223"
-            displaystart="120"
-            displayend="158"
-            highlightStart="10"
-            highlightEnd="45"
-          />
-          <protvista-sequence
-            id="seq1"
-            length="223"
-            displaystart="120"
-            displayend="158"
-          />
-          <protvista-track
-            id="track1"
-            length="223"
-            displaystart="120"
-            displayend="158"
-          >
+          <protvista-navigation length="770" />
+          <protvista-sequence length="770" id="sequence-track" />
+          <protvista-track id="track1" length="770">
             <protvista-feature-adapter id="adapter1">
               <data-loader>
                 <source src="https://www.ebi.ac.uk/proteins/api/features/P05067?categories=PTM" />
               </data-loader>
             </protvista-feature-adapter>
           </protvista-track>
-          <protvista-track
-            id="track2"
-            length="223"
-            displaystart="120"
-            displayend="158"
-            highlightstart="10"
-            highlightend="45"
-            layout="non-overlapping"
-          />
+          <protvista-track id="track1" length="770" layout="non-overlapping">
+            <protvista-feature-adapter id="adapter1">
+              <data-loader>
+                <source src="https://www.ebi.ac.uk/proteins/api/features/P05067?categories=PTM" />
+              </data-loader>
+            </protvista-feature-adapter>
+          </protvista-track>
           <protvista-interpro-track
-            id="track3"
-            length="223"
-            displaystart="120"
-            displayend="158"
-            highlightstart="10"
-            highlightend="45"
+            id="interpro-track"
+            length="770"
+            shape="roundRectangle"
+            expanded
           />
-          <protvista-variation
-            id="variation-track"
-            length="223"
-            displaystart="1"
-            displayend="158"
-            highlightstart="10"
-            highlightend="45"
-          />
+          <protvista-variation id="variation-track" length="770" />
         </protvista-manager>
       </Fragment>
     );
