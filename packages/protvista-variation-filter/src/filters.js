416c97ddc31d52515d91e8f4ece6b49a3ffcb5d0
Xavier Watkins <xavier.watkins@gmail.com>
2018-12-04 09:31:24 +0000

    Changes to uniprot component to make it build

---
+++ packages/protvista-variation-filter/src/filters.js
@@ -0,0 +1,72 @@
+import cloneDeep from 'lodash-es/cloneDeep';
+// import {filter} from 'bluebird';
+
+const filters = [
+    {
+        name: 'disease',
+        color: ['#990000'],
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => {
+                return (variant.association && variant.association.some(d => d.disease)) || (variant.clinicalSignificances && variant.clinicalSignificances === 'disease')
+            }));
+            return filteredData;
+        }
+    }, {
+        name: 'predicted',
+        color: [
+            '#002594', '#8FE3FF'
+        ],
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => variant.sourceType === 'large_scale_study'));
+            return filteredData;
+        }
+    }, {
+        name: 'nonDisease',
+        color: ['#99cc00'],
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => {
+                return (variant.association && variant.association.some(d => !d.disease)) || (variant.clinicalSignificances && variant.clinicalSignificances === 'likely benign')
+            }));
+            return filteredData;
+        }
+    }, {
+        name: 'uncertain',
+        color: '#FFCC00',
+        applyFilter: data => {}
+    }, {
+        name: 'UniProt',
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => variant.xrefNames.includes('uniprot')));
+            return filteredData;
+        }
+    }, {
+        name: 'ClinVar',
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => variant.xrefNames.includes('clinvar')));
+            return filteredData;
+        }
+    }, {
+        name: 'LSS',
+        applyFilter: data => {
+            const filteredData = cloneDeep(data);
+            filteredData.forEach(variants => variants.variants = variants.variants.filter(variant => variant.sourceType === 'large_scale_study' || variant.sourceType === 'mixed'));
+            return filteredData;
+        }
+    }
+];
+
+function getFiltersFromAttribute(attrValue) {
+    if (attrValue) {
+        const filterStrings = attrValue.split(',');
+        return filters.filter(f => filterStrings.find(d => d === f.name));
+    } else {
+        return [];
+    }
+}
+
+export {getFiltersFromAttribute};
