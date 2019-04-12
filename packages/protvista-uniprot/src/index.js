879b80bdc2dbefc00c11c8b61cca1553ee070c06
Xavier Watkins <xavier.watkins@gmail.com>
2019-04-12 14:36:48 +0100

    Cleanup components

--- packages/protvista-uniprot/src/index.js
+++
@@ -1,6 +0,0 @@
-import ProtvistaUniprot from "./protvista-uniprot";
-import { loadComponent } from "./loadComponents";
-
-loadComponent("protvista-uniprot", ProtvistaUniprot);
-
-export default ProtvistaUniprot;
