c2cc12b30f3e51a8ff98ed99c09d7feddc966707
Xavier Watkins <xavier.watkins@gmail.com>
2019-05-24 10:52:43 +0100

    Issues with filter, make datatable height an attr

--- packages/protvista-datatable/src/protvista-datatable.js
+++ packages/protvista-datatable/src/protvista-datatable.js
@@ -4,6 +4,7 @@ import { v1 } from "uuid";
 class ProtvistaDatatable extends LitElement {
   constructor() {
     super();
+    this.height = 15;
     this.eventHandler = this.eventHandler.bind(this);
   }
 
@@ -53,6 +54,7 @@ class ProtvistaDatatable extends LitElement {
           }
         }
       },
+      height: { type: Number },
       columns: { type: Object },
       displayStart: { type: Number },
       displayEnd: { type: Number },
@@ -74,7 +76,8 @@ class ProtvistaDatatable extends LitElement {
 
       th {
         text-align: left;
-        color: var(--protvista-datable__header-color, #393b42);
+        background-color: var(--protvista-datable__header-background, #fff);
+        color: var(--protvista-datable__header-text, #393b42);
         text-overflow: ellipsis;
       }
 
@@ -91,7 +94,6 @@ class ProtvistaDatatable extends LitElement {
       tbody {
         display: block;
         width: 100%;
-        height: 15rem;
         overflow-y: auto;
         overflow-x: hidden;
       }
@@ -209,7 +211,7 @@ class ProtvistaDatatable extends LitElement {
             )}
           </tr>
         </thead>
-        <tbody>
+        <tbody style="height:${this.height}rem">
           ${this.data.map(
             row =>
               html`
--- packages/protvista-filter/src/checkbox.js
+++ packages/protvista-filter/src/checkbox.js
@@ -1,11 +1,10 @@
 import { html, render } from "lit-html";
 
-import './style.css';
-
+import "./style.css";
 
 class ProtvistaCheckbox extends HTMLElement {
   static get tagName() {
-    return 'protvista-checkbox';
+    return "protvista-checkbox";
   }
 
   constructor() {
@@ -18,54 +17,80 @@ class ProtvistaCheckbox extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ['checked', 'disabled', 'value'];
+    return ["checked", "disabled", "value"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
-      case 'checked':
-        this.checked  = true;
+      case "checked":
+        this.checked = true;
         break;
-      case 'disabled':
+      case "disabled":
         this.disabled = true;
         break;
-      case 'value':
+      case "value":
         this.value = newValue;
     }
   }
 
   _render() {
-    let { value, options: {labels, colors}, checked=false, disabled=false } = this;
+    let {
+      value,
+      options: { labels, colors },
+      checked = false,
+      disabled = false
+    } = this;
     if (colors.length == null) {
       colors = [colors];
     }
-    value = `filter-${value.split(':')[1]}`;
+    value = `filter-${value.split(":")[1]}`;
+    const cssId = value.replace(/[\s,\(\)']/g, "");
     const isCompound = colors.length > 1;
-    render(html`
-      <style>
-        #${value} .protvista_checkbox_input + .protvista_checkbox_label::before {
-          background: ${isCompound ? html`linear-gradient(${colors[0]}, ${colors[1]})` : colors[0]};
-          opacity: 0.4;
-        }
+    render(
+      html`
+        <style>
+          #${cssId}
+            .protvista_checkbox_input
+            + .protvista_checkbox_label::before {
+            background: ${isCompound
+              ? html`
+                  linear-gradient(${colors[0]}, ${colors[1]})
+                `
+              : colors[0]};
+            opacity: 0.4;
+          }
 
-        #${value} .protvista_checkbox_input:checked + .protvista_checkbox_label::before {
-          opacity: 1;
-        }
-      </style>
-      <label id="${value}"
-             class="protvista_checkbox ${isCompound ? 'compound' : ''}"
-             tabindex="0">
-        <input type="checkbox"
-              class="protvista_checkbox_input"
-              ?checked="${checked}"
-              ?disabled="${disabled}"
-              .value="${value}"
-              @change="${this._toggleChecked}">
-        <span class="protvista_checkbox_label">
-          ${labels.map(l => html`<span>${l}</span>`)}
-        </span>
-      </label>
-    `, this);
+          #${cssId}
+            .protvista_checkbox_input:checked
+            + .protvista_checkbox_label::before {
+            opacity: 1;
+          }
+        </style>
+        <label
+          id="${cssId}"
+          class="protvista_checkbox ${isCompound ? "compound" : ""}"
+          tabindex="0"
+        >
+          <input
+            type="checkbox"
+            class="protvista_checkbox_input"
+            ?checked="${checked}"
+            ?disabled="${disabled}"
+            .value="${value}"
+            @change="${this._toggleChecked}"
+          />
+          <span class="protvista_checkbox_label">
+            ${labels.map(
+              l =>
+                html`
+                  <span>${l}</span>
+                `
+            )}
+          </span>
+        </label>
+      `,
+      this
+    );
   }
 
   _toggleChecked(event) {
@@ -76,14 +101,16 @@ class ProtvistaCheckbox extends HTMLElement {
   }
 
   _fireEvent() {
-    this.dispatchEvent(new CustomEvent('filterChange', {
-      bubbles: true,
-      composed: true,
-      detail: {
-        checked: this.checked,
-        value: this.value
-      }
-    }));
+    this.dispatchEvent(
+      new CustomEvent("filterChange", {
+        bubbles: true,
+        composed: true,
+        detail: {
+          checked: this.checked,
+          value: this.value
+        }
+      })
+    );
   }
 }
 
