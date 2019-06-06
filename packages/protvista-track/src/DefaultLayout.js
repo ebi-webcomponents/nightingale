92cd19f5317b1e0f54246c38f5057789bf548692
Xavier Watkins <xavier.watkins@gmail.com>
2019-06-06 11:03:07 +0100

    formatting

--- packages/protvista-track/src/DefaultLayout.js
+++ packages/protvista-track/src/DefaultLayout.js
@@ -1,21 +1,16 @@
 export default class DefaultLayout {
-  constructor({
-    features,
-    layoutHeight,
-    padding = 1,
-    minHeight = 17,
-  }) {
+  constructor({ layoutHeight, padding = 1, minHeight = 17 }) {
     this._padding = padding;
     this._minHeight = minHeight;
     this._layoutHeight = layoutHeight;
   }
-  init(features){
+  init(features) {
     this._features = features;
-  };
+  }
   getFeatureYPos(feature) {
     return Math.min(this._layoutHeight, this._minHeight);
   }
   getFeatureHeight(feature) {
     return Math.min(this._layoutHeight, this._minHeight);
-  };
+  }
 }
