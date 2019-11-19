2523f68f456d9cabca6b0dac39b2fab7429dcee3
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-11-19 14:36:47 +0100

    code formatting

--- app/src/tutorial/GettingStarted.jsx
+++ app/src/tutorial/GettingStarted.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef } from "react";
 
-import hljs from 'highlight.js';
-import javascript from 'highlight.js/lib/languages/javascript';
-import 'highlight.js/styles/github.css';
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/github.css";
 
 import "./GettingStarted.css";
 
-import htmlContent from '../../../GETTING_STARTED.md';
+import htmlContent from "../../../GETTING_STARTED.md";
 
-hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage("javascript", javascript);
 
-const dangerouslySetInnerHTML = {__html: htmlContent};
+const dangerouslySetInnerHTML = { __html: htmlContent };
 
 const GettingStarted = () => {
   const ref = useRef();
 
   useEffect(() => {
     if (!ref.current) return;
-    for (const codeBlock of ref.current.querySelectorAll('pre code')) {
+    for (const codeBlock of ref.current.querySelectorAll("pre code")) {
       hljs.highlightBlock(codeBlock);
     }
   }, []);
