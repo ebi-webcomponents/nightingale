5822221925186a82ad4fb80020cb8a23b5485a86
Xavier Watkins <xavier.watkins@gmail.com>
2019-11-19 15:27:36 +0100

    Refactor to expose transformData

--- packages/protvista-feature-adapter/src/BasicHelper.js
+++ packages/protvista-feature-adapter/src/BasicHelper.js
@@ -1,29 +1,27 @@
 import ecoMap from "./evidences";
 
-class BasicHelper {
-  // TODO this would be better done in the data setter?
-  static renameProperties(features) {
-    return features.map(ft => {
-      const obj = {};
-      if (ft.begin) {
-        obj.start = ft.begin;
-      }
-      return {
-        ...ft,
-        ...obj
-      };
-    });
-  }
+export const renameProperties = features => {
+  return features.map(ft => {
+    const obj = {};
+    if (ft.begin) {
+      obj.start = ft.begin;
+    }
+    return {
+      ...ft,
+      ...obj
+    };
+  });
+};
 
-  static formatSource(source) {
-    return source.name.toLowerCase() === "PubMed".toLowerCase()
-      ? `${source.id}&nbsp;(<a href='${source.url}' style="color:#FFF" target='_blank'>${source.name}</a>&nbsp;<a href='${source.alternativeUrl}' style="color:#FFF" target='_blank'>EuropePMC</a>)`
-      : `&nbsp;<a href='${source.url}' style="color:#FFF" target='_blank'>${source.id}</a>&nbsp;(${source.name})`;
-  }
+const formatSource = source => {
+  return source.name.toLowerCase() === "PubMed".toLowerCase()
+    ? `${source.id}&nbsp;(<a href='${source.url}' style="color:#FFF" target='_blank'>${source.name}</a>&nbsp;<a href='${source.alternativeUrl}' style="color:#FFF" target='_blank'>EuropePMC</a>)`
+    : `&nbsp;<a href='${source.url}' style="color:#FFF" target='_blank'>${source.id}</a>&nbsp;(${source.name})`;
+};
 
-  static getEvidenceFromCodes(evidenceList) {
-    if (!evidenceList) return ``;
-    return `
+const getEvidenceFromCodes = evidenceList => {
+  if (!evidenceList) return ``;
+  return `
       <ul>${evidenceList
         .map(ev => {
           const ecoMatch = ecoMap.find(eco => eco.name === ev.code);
@@ -31,29 +29,29 @@ class BasicHelper {
           return `<li title='${
             ecoMatch.description
           }' style="padding: .25rem 0">${ecoMatch.shortDescription}:&nbsp;${
-            ev.source ? BasicHelper.formatSource(ev.source) : ""
+            ev.source ? formatSource(ev.source) : ""
           }</li>`;
         })
         .join("")}</ul>
     `;
-  }
+};
 
-  static formatXrefs(xrefs) {
-    return `<ul>${xrefs
-      .map(
-        xref =>
-          `<li style="padding: .25rem 0">${xref.name} ${
-            xref.url
-              ? `<a href="${xref.url}" style="color:#FFF" target="_blank">${xref.id}</a>`
-              : `${xref.name} ${xref.id}`
-          }</li>`
-      )
-      .join("")}</ul>`;
-  }
+const formatXrefs = xrefs => {
+  return `<ul>${xrefs
+    .map(
+      xref =>
+        `<li style="padding: .25rem 0">${xref.name} ${
+          xref.url
+            ? `<a href="${xref.url}" style="color:#FFF" target="_blank">${xref.id}</a>`
+            : `${xref.name} ${xref.id}`
+        }</li>`
+    )
+    .join("")}</ul>`;
+};
 
-  static formatTooltip(feature) {
-    const evidenceHTML = BasicHelper.getEvidenceFromCodes(feature.evidences);
-    return `
+export const formatTooltip = feature => {
+  const evidenceHTML = getEvidenceFromCodes(feature.evidences);
+  return `
       ${
         feature.description
           ? `<h5>Description</h5><p>${feature.description}</p>`
@@ -73,11 +71,8 @@ class BasicHelper {
       ${evidenceHTML ? `<h5>Evidence</h5>${evidenceHTML}` : ``}
       ${
         feature.xrefs
-          ? `<h5>Cross-references</h5>${BasicHelper.formatXrefs(feature.xrefs)}`
+          ? `<h5>Cross-references</h5>${formatXrefs(feature.xrefs)}`
           : ""
       }
         `;
-  }
-}
-
-export default BasicHelper;
+};
