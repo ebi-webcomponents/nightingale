b2b73aeb22ea0ecc9e9e8514967e1f619ef876a8
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2020-06-05 09:40:52 +0100

    better defining react externals

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,18 @@ const config = {
   externals: {
     d3: "d3",
     litemol: "LiteMol",
-    react: "react",
-    "react-dom": "react-dom",
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom"
+    },
     "protvista-zoomable": "ProtvistaZoomable",
     "protvista-track": "ProtvistaTrack",
     "protvista-feature-adapter": "ProtvistaFeatureAdapter",
