9811f8fa40989f0062789421f86cced1896188ff
Xavier Watkins <xavier.watkins@gmail.com>
2020-09-10 16:51:00 +0100

    Cleanup event listener

--- app/src/components/ProtvistaVariation.jsx
+++ app/src/components/ProtvistaVariation.jsx
@@ -9,15 +9,20 @@ import readmeContent from "../../../packages/protvista-variation/README.md";
 const ProtvistaVariationWrapper = () => {
   const [tooltipContent, setTooltipContent] = useState();
 
+  const handleEvent = (e) => {
+    if (e.detail.eventtype === "click") {
+      setTooltipContent({ __html: e.detail.feature.tooltipContent });
+    }
+  };
+
   useEffect(() => {
     loadWebComponent("protvista-variation", ProtvistaVariation);
     loadWebComponent("data-loader", DataLoader);
     loadWebComponent("protvista-variation-adapter", ProtvistaVariationAdapter);
-    window.addEventListener("change", (e) => {
-      if (e.detail.eventtype === "click") {
-        setTooltipContent({ __html: e.detail.feature.tooltipContent });
-      }
-    });
+    window.addEventListener("change", handleEvent);
+    return () => {
+      window.removeEventListener("change", handleEvent);
+    };
   }, []);
 
   return (
