c011f7b1954628a48ac08c3fa29c98132f5fd655
Veronika Scheuerová <veronikascheuerova@gmail.com>
2021-05-10 11:21:33 +0200

    added radix parameter

--- packages/protvista-sequence/src/protvista-sequence.js
+++ packages/protvista-sequence/src/protvista-sequence.js
@@ -9,7 +9,7 @@ class ProtVistaSequence extends ProtvistaZoomable {
     super.connectedCallback();
     this.sequence = this.getAttribute("sequence");
     const ticks = this.getAttribute("numberofticks");
-    this.numberOfTicks = Number.isInteger(parseInt(ticks))
+    this.numberOfTicks = Number.isInteger(parseInt(ticks, 10))
       ? ticks
       : DEFAULT_NUMBER_OF_TICKS;
     if (this.sequence) {
