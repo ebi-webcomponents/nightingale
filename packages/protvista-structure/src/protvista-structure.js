54a5a91cd4366982d56927fea7f85beddebe1211
Daniel Rice <dlrice@ebi.ac.uk>
2021-05-27 15:35:50 +0100

    Remove litemol.

--- packages/protvista-structure/src/protvista-structure.js
+++ packages/protvista-structure/src/protvista-structure.js
@@ -16,10 +16,6 @@ class ProtvistaStructure extends HTMLElement {
 
   get css() {
     return `
-    :root {
-      --blue: 0, 112, 155;
-    }
-  
     protvista-structure h4 {
       display: inline;
       margin-right: 1em;
@@ -28,10 +24,6 @@ class ProtvistaStructure extends HTMLElement {
     .molstar-container {
       position: relative;
       height: ${this._height};
-    }
-
-    .lm-viewport-controls {
-      display: ${this.hideViewportControls ? "none" : "block"};
     }
     `;
   }
@@ -56,10 +48,6 @@ class ProtvistaStructure extends HTMLElement {
 
   get height() {
     return this.getAttribute("height");
-  }
-
-  get hideViewportControls() {
-    return this.getAttribute("hide-viewport-controls");
   }
 
   connectedCallback() {
@@ -181,133 +169,12 @@ class ProtvistaStructure extends HTMLElement {
     const mappings = Object.values(pdbEntry)[0].UniProt[this._accession]
       ?.mappings;
 
-    // await this._molStar.loadPdb("1grm".toLowerCase());
     await this._molStar.loadPdb(id.toLowerCase());
     this._selectedMolecule = {
       id,
       mappings,
     };
     this._planHighlight();
-  }
-
-  // loadMolStar() {
-  // this.Event.Molecule.ModelSelect.getStream(
-  //   this._liteMol.context
-  // ).subscribe((e) => this.propagateHighlight(e));
-  // }
-
-  // getTheme() {
-  // TODO:  Configure selection and highlight colors when Mol* supports this
-  //        https://github.com/molstar/molstar/blob/410655052fa9b93c2d205d3528bc36914753dd4d/src/mol-plugin-ui/sequence/sequence.tsx#L154
-  //   const colors = new Map();
-  //   colors.set("Uniform", this.CoreVis.Color.fromRgb(207, 178, 178));
-  //   colors.set("Selection", this.CoreVis.Color.fromRgb(255, 0, 0));
-  //   colors.set("Highlight", this.CoreVis.Theme.Default.HighlightColor);
-  //   return this.Visualization.Molecule.uniformThemeProvider(undefined, {
-  //     colors,
-  //   });
-  // }
-
-  loadLiteMol() {
-    const { Plugin } = LiteMol;
-    this.Command = LiteMol.Bootstrap.Command;
-    this.Query = LiteMol.Core.Structure.Query;
-    this.Bootstrap = LiteMol.Bootstrap;
-    this.Core = LiteMol.Core;
-    this.Tree = this.Bootstrap.Tree;
-    this.CoreVis = LiteMol.Visualization;
-    this.Transformer = this.Bootstrap.Entity.Transformer;
-    this.Visualization = this.Bootstrap.Visualization;
-    this.Event = this.Bootstrap.Event;
-    this.Context = Plugin.Components.Context;
-
-    this._liteMol = Plugin.create({
-      target: this.querySelector("#litemol-instance"),
-      viewportBackground: "#fff",
-      layoutState: {
-        hideControls: true,
-      },
-      allowAnalytics: false,
-    });
-
-    // this.Event.Molecule.ModelSelect.getStream(
-    //   this._liteMol.context
-    // ).subscribe((e) => this.propagateHighlight(e));
-  }
-
-  loadMolecule(_id) {
-    this._liteMol.clear();
-
-    const transform = this._liteMol.createTransform();
-
-    transform
-      .add(this._liteMol.root, this.Transformer.Data.Download, {
-        url: `https://www.ebi.ac.uk/pdbe/coordinates/${_id.toLowerCase()}/full?encoding=BCIF`,
-        type: "Binary",
-        _id,
-      })
-      .then(
-        this.Transformer.Data.ParseBinaryCif,
-        {
-          id: _id,
-        },
-        {
-          isBinding: true,
-          ref: "cifDict",
-        }
-      )
-      .then(
-        this.Transformer.Molecule.CreateFromMmCif,
-        {
-          blockIndex: 0,
-        },
-        {
-          isBinding: true,
-        }
-      )
-      .then(
-        this.Transformer.Molecule.CreateModel,
-        {
-          modelIndex: 0,
-        },
-        {
-          isBinding: false,
-          ref: "model",
-        }
-      )
-      .then(this.Transformer.Molecule.CreateMacromoleculeVisual, {
-        polymer: true,
-        polymerRef: "polymer-visual",
-        het: true,
-        water: true,
-      });
-
-    return this._liteMol.applyTransform(transform);
-  }
-
-  getTheme() {
-    const colors = new Map();
-    colors.set("Uniform", this.CoreVis.Color.fromRgb(207, 178, 178));
-    colors.set("Selection", this.CoreVis.Color.fromRgb(255, 0, 0));
-    colors.set("Highlight", this.CoreVis.Theme.Default.HighlightColor);
-    return this.Visualization.Molecule.uniformThemeProvider(undefined, {
-      colors,
-    });
-  }
-
-  addMessage(message) {
-    this.removeMessage();
-    this._liteMol.command(this.Bootstrap.Command.Toast.Show, {
-      key: "UPMessage",
-      message,
-      timeoutMs: 30 * 1000,
-    });
-  }
-
-  removeMessage() {
-    this._liteMol.command(this.Bootstrap.Command.Toast.Hide, {
-      key: "UPMessage",
-    });
   }
 
   processMapping(mappingData) {
@@ -395,42 +262,6 @@ class ProtvistaStructure extends HTMLElement {
 
     this._molStar.highlight(translatedPositions);
     this._molStar.clearMessages();
-    // const queries = translatedPositions.map((translatedPos) =>
-    //   this.Query.sequence(
-    //     translatedPos.entity.toString(),
-    //     translatedPos.chain,
-    //     {
-    //       seqNumber: translatedPos.start,
-    //     },
-    //     {
-    //       seqNumber: translatedPos.end,
-    //     }
-    //   )
-    // );
-
-    // const theme = this.getTheme();
-    // const transform = this._liteMol.createTransform();
-
-    // queries.forEach((query) =>
-    //   transform.add(
-    //     visual,
-    //     this.Transformer.Molecule.CreateSelectionFromQuery,
-    //     {
-    //       query,
-    //     },
-    //     {
-    //       ref: "sequence-selection",
-    //     }
-    //   )
-    // );
-
-    // this._liteMol.applyTransform(transform).then(() => {
-    //   this.Command.Visual.UpdateBasicTheme.dispatch(this._liteMol.context, {
-    //     visual,
-    //     theme,
-    //   });
-    // });
-    // this.removeMessage();
   }
 }
 
