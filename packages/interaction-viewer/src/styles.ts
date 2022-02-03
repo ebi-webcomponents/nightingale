e7fbc60fdaf6dee8d92483718b604ec061d07ea8
Xavier Watkins <xavier.watkins@gmail.com>
2022-02-03 13:49:23 +0000

    tidy up

--- packages/interaction-viewer/src/styles.ts
+++ packages/interaction-viewer/src/styles.ts
@@ -5,6 +5,10 @@ const styles = css`
     position: relative;
   }
 
+  :host #container {
+    margin-top: 1rem;
+  }
+
   :host text {
     font-family: "Open Sans", sans-serif;
     fill: black;
@@ -18,77 +22,6 @@ const styles = css`
 
   :host .active-row {
     stroke: #4a90e2;
-  }
-
-  :host .interaction-tooltip {
-    z-index: 99999;
-    position: absolute;
-    background: #ffffff;
-    padding: 0.5em 1em;
-    border: 1px #979797 solid;
-    box-shadow: 2px 2px 2px #333333;
-    -webkit-transition: all 0.5s;
-    /* Safari */
-    transition: all 0.25s;
-    min-width: 36em;
-  }
-
-  :host .interaction-tooltip .close-interaction-tooltip {
-    cursor: pointer;
-    float: right;
-    margin-bottom: 0.8em;
-  }
-
-  :host .interaction-tooltip .tooltip-content {
-    clear: both;
-  }
-
-  :host .interaction-filter-container {
-    text-align: left;
-  }
-
-  :host .interaction-filter-container #filter-display .filter-selected {
-    margin: 0.2em 0.5em;
-    padding: 0.3em 0.1em;
-    background-color: #f2f2f2;
-    border: 1px solid #e8e8e8;
-    cursor: pointer;
-    white-space: nowrap;
-    display: inline-block;
-  }
-
-  :host .interaction-filter-container #filter-display .filter-selected::after {
-    content: "✖";
-    margin: 0 0.3em;
-  }
-
-  :host .interaction-filter-container .interaction-filter {
-    vertical-align: top;
-    margin-bottom: 0.5em;
-    display: inline-block;
-  }
-
-  :host .interaction-filter-container .interaction-filter ul {
-    border: #e8e8e8 1px solid;
-    max-height: 15em;
-    overflow-y: auto;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  :host .interaction-filter-container .interaction-filter ul li {
-    cursor: pointer;
-    padding: 0.5em 0.5em;
-    border-bottom: #e8e8e8 1px solid;
-  }
-
-  :host .interaction-filter-container .interaction-filter ul li:hover {
-    background-color: #f2f2f2;
-  }
-
-  :host .interaction-filter-container .interaction-filter ul li.active {
-    font-weight: bold;
   }
 
   :host .cell {
