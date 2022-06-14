404f8f797425d89a3de36e3924d6abdd0804fb83
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-06-14 14:43:03 +0100

    adding stories for nightingale-sequence

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
+    "@storybook/addon-storysource",
   ],
   typescript: {
     check: false,
