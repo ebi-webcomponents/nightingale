65f83c4e62e2a8f074a45c83ee810545bb80c8af
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-06-22 10:01:15 +0100

    nightingale-interpro-track: fixing tests

--- packages/nightingale-interpro-track/tests/nightingale-interpro-track.test.js
+++ packages/nightingale-interpro-track/tests/nightingale-interpro-track.test.js
@@ -1,14 +1,14 @@
 import "../src/index";
-import * as iproData from "../packages/nightingale-interpro-track/tests/mockData/interpro-IPR016039.json";
-import * as contributors from "../packages/nightingale-interpro-track/tests/mockData/interpro-contributors.json";
-import * as residues from "../packages/nightingale-interpro-track/tests/mockData/interpro-residues.json";
+import * as iproData from "./mockData/interpro-IPR016039.json";
+import * as contributors from "./mockData/interpro-contributors.json";
+import * as residues from "./mockData/interpro-residues.json";
 
 let rendered;
 
 describe("nightingale-interpro-track tests", () => {
   beforeEach(async () => {
     document.documentElement.innerHTML = `<nightingale-interpro-track length="450" height="80"></nightingale-interpro-track>`;
-    rendered = document.querySelector("nightingale-track");
+    rendered = document.querySelector("nightingale-interpro-track");
     await new Promise((resolve) => requestAnimationFrame(() => resolve()));
     await rendered.updateComplete;
     rendered.data = iproData;
@@ -17,7 +17,7 @@ describe("nightingale-interpro-track tests", () => {
   });
 
   afterEach(() => {
-    document.querySelector("nightingale-track").remove();
+    document.querySelector("nightingale-interpro-track").remove();
   });
 
   test("it should display the track correctly", async () => {
