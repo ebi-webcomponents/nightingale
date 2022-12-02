bb9790411204169e651ec4f58a5507d95e22a8d0
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-12-02 10:33:33 +0000

    nightingale-saver: migration

---
+++ packages/nightingale-saver/README.md
@@ -0,0 +1,84 @@
+[![Published on NPM](https://img.shields.io/npm/v/@nightingale-elements/nightingale-saver.svg)](https://www.npmjs.com/package/@nightingale-elements/nightingale-saver)
+
+## &lt;nightingale-saver&gt;
+
+The `nightingale-saver` component is used to download the ProtVista svg as images. By default, it renders a download button. It can be modified to include
+different content although the first child element provided should be a button.
+
+The component to be saved is bound to protvista-saver though the `element-id` attribute.
+
+The background color is none by default. It can be set through the `background-color` attribute. The name and format of the file to be downloaded are
+passed as `file-name` and `file-format` respectively.
+
+There are two methods available : `preSave()` and `postSave()`. These functions allows any manipulations to DOM before and after painting on the canvas.
+
+[Demo](https://ebi-webcomponents.github.io/nightingale/?path=/story/components-manager--manager)
+
+## Usage
+
+#### Basic
+
+```html
+<nightingale-saver element-id="track" id="#saver"> </nightingale-saver>
+```
+
+#### Passing different content to render other than the default
+
+```html
+<nightingale-saver
+  element-id="just-tracks"
+  file-name="tracks"
+  file-format="jpeg"
+  background-color="#ddddee"
+>
+  <button>Download Just Tracks</button>
+</nightingale-saver>
+```
+
+#### Using preSave() and postSave()
+
+```javascript
+document.querySelector("#saver").preSave = () => {
+  const base = document.querySelector("#track");
+  const title = document.createElement("h2");
+  title.setAttribute("id", "tmp_title_element");
+  title.innerHTML = "ProtVista Snapshot";
+  base.insertBefore(title, base.firstChild);
+};
+//removes the title from the DOM
+document.querySelector("#saver").postSave = () => {
+  document
+    .querySelector("#track")
+    .removeChild(document.getElementById("tmp_title_element"));
+};
+```
+
+## API Reference
+
+### Attributes
+
+#### `element-id: string`
+
+The Id of the component to be saved.
+
+#### `background-color: string`
+
+The background color for the canvas.
+
+#### `file-name: string`
+
+The file name to be used to save locally.
+
+#### `file-format: string`
+
+The format of the image. Accepted formats are png, jpeg, bmp, tiff and gif.
+
+### Properties
+
+#### `preSave: () => void `
+
+The DOM manipulation if needed to be done before printing on the canvas.
+
+#### `postSave: () => void `
+
+The things that were changed during the preSave needed to be reverted in the postSave.
---
+++ packages/nightingale-saver/src/nightingale-saver.ts
@@ -0,0 +1,128 @@
+import { html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import * as rasterizeHTML from "rasterizehtml";
+import NightingaleElement from "@nightingale-elements/nightingale-new-core";
+
+// Accepted file formats
+const formats = ["png", "jpeg", "bmp", "tiff", "gif"];
+
+@customElement("nightingale-saver")
+class NightingaleSaver extends NightingaleElement {
+  @property({ type: String, attribute: "element-id" })
+  elementId = "";
+  @property({ type: String, attribute: "background-color" })
+  fillColor = "transparent";
+  @property({ type: String, attribute: "file-name" })
+  fileName = "nightingale.snapshot";
+  @property({
+    type: String,
+    attribute: "file-format",
+    converter: (value) => (formats.includes(value || "") ? value : "png"),
+  })
+  fileFormat = "png";
+
+  preSave?: () => void = undefined;
+  postSave?: () => void = undefined;
+
+  constructor() {
+    super();
+    this.saveSvg = this.saveSvg.bind(this);
+  }
+  saveSvg() {
+    const element = document.querySelector<HTMLElement>(`#${this.elementId}`);
+    if (!element) {
+      throw new Error(
+        "The 'element-id' attribute is not related with a current DOM element"
+      );
+    }
+    element.style.display = "block"; // In order to get the width and height of protvista manager, its display has to be set to block
+
+    // PreSave function allows to do anything before actually printing on the canvas
+    if (typeof this.preSave === "function") {
+      this.preSave();
+    }
+    const { width, height } = element.getBoundingClientRect();
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("width", `${width}px`);
+    canvas.setAttribute("height", `${height}px`);
+    if (this.fillColor) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.fillStyle = this.fillColor;
+        context.fillRect(0, 0, width, height);
+      }
+    }
+    // uncomment for development and see the canvas on screen
+    // element.appendChild(canvas);
+
+    // Rendering the Protvista svg
+    rasterizeHTML
+      .drawHTML(wrapHTML(element.innerHTML), canvas)
+      .then(() => {
+        const image = canvas
+          .toDataURL(`image/${this.fileFormat}`, 1.0)
+          .replace(`image/${this.fileFormat}`, "image/octet-stream");
+        const link = document.createElement("a");
+        link.download = `${this.fileName}.${this.fileFormat}`;
+        link.href = image;
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new Error(
+          `Couldn't generate the snapshot for the element '${this.elementId}'`
+        );
+      })
+      .finally(() => {
+        element.style.display = ""; // Display property is set back to its original value
+        // Reverting the changes to the dom if it had been changed during the preSave
+        if (typeof this.postSave === "function") {
+          this.postSave();
+        }
+      });
+  }
+
+  render() {
+    /* The component only supports button as child and it has to be the first child element. If not, an error is thrown. By default,
+        a save button is shown if nothing is specified. */
+    let button;
+    // const save = () => this.saveSvg();
+    if (this.hasChildNodes()) {
+      const child = this.firstElementChild;
+      if (child?.tagName === "BUTTON") {
+        child.addEventListener("click", this.saveSvg);
+        button = html` ${child} `;
+      } else {
+        console.error("Only Buttons are accepted as children");
+      }
+    } else {
+      button = html`
+        <button
+          style="background-color: #1878ba; color: white; cursor: pointer; display: inline-block;padding: 0.5rem 0.5rem; border-radius: 4px; margin-bottom: 5px;"
+          @click=${this.saveSvg}
+        >
+          Download
+        </button>
+      `;
+    }
+    return button;
+  }
+}
+
+export default NightingaleSaver;
+
+const wrapHTML = (html: string) =>
+  `
+<html>
+  <head>
+    <style>
+      html, body {
+        padding: 0;
+        margin: 0;
+      }
+    </style>
+  </head>
+  <body>${html}</body>
+</html>
+`;
