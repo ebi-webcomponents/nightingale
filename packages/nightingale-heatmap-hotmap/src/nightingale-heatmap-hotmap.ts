a2c8dab88fd563ee23c2d6a3baf789cf76c5891b
Marcelo Afonso <marcelo@ebi.ac.uk>
2024-04-30 10:40:04 +0100

    hello world & documented lifecycle in new comp

--- packages/nightingale-heatmap-hotmap/src/nightingale-heatmap-hotmap.ts
+++ packages/nightingale-heatmap-hotmap/src/nightingale-heatmap-hotmap.ts
@@ -1,3 +1,4 @@
+import { html } from "lit";
 import NightingaleElement, { customElementOnce, withDimensions, withHighlight, withManager, withMargin, withPosition, withResizable, withZoom } from "@nightingale-elements/nightingale-new-core";
 
 // TODO: height is not triggering a full redrawn when is changed after first render
@@ -13,5 +14,60 @@ class NightingaleHeatmapHotmap extends withManager(
     ),
   ),
 ) {
+
+  /**
+   * 1st ON CREATED: Called once for each attribute on order they appear
+   * 4th ON CREATED: Additional or duplicate call based on Mixins (see below:)
+   *     - withResizable adds min-width, min-height
+   *     - withHighlight duplicate set highlight-color
+   *     - withPosition duplicate set display-start and display-end
+   * 
+   * 1st ON UPDATED: Called once by each attribute updated by manager
+   */
+  attributeChangedCallback(
+    name: string,
+    oldValue: string | null,
+    newValue: string | null,
+  ): void {
+    // console.log("attributeChangedCallback")
+    super.attributeChangedCallback(name, oldValue, newValue);
+    if (
+      ATTRIBUTES_THAT_TRIGGER_REFRESH.includes(name) ||
+      name.startsWith("margin-")
+    ) {
+      // console.log("FOR: " + ATTRIBUTES_THAT_TRIGGER_REFRESH.toString())
+    }
+    // console.log(name, oldValue, newValue)
+    // console.log("")
+  }
+
+  /**
+   * 2nd ON CREATED
+   */
+  connectedCallback() {
+    // console.log("connectedCallback")
+    // console.log("")
+    super.connectedCallback();
+  }
+
+  /**
+   * 3rd ON CREATED
+   * 
+   * 2nd ON UPDATED
+   */
+  render() {
+    // console.log("render")
+    // console.log("")
+    return html`<h1 class="hello-world-hotmap">Hi I am the hotmap container</h1>`;
+  }
+
+  /**
+   * 5TH (last) ON CREATED (1st is also 4th)
+   */
+  firstUpdated() {
+    // console.log("firstUpdated")
+    // console.log("")
+  }
+ 
 }
 export default NightingaleHeatmapHotmap;
