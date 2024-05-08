fb31cd01078431280868726c6dd41a8892c7b888
Marcelo Afonso <marcelo@ebi.ac.uk>
2024-05-08 14:59:11 +0100

    removed old hotmap test.js

--- packages/nightingale-sequence-heatmap/tests/nightingale-heatmap-hotmap.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const nightingaleHeatmapHotmap = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(nightingaleHeatmapHotmap(), 'Hello from nightingaleHeatmapHotmap');
-console.info('nightingaleHeatmapHotmap tests passed');
