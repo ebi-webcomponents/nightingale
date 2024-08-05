e61c96d4e416419581d85a9a949afbc822146723
Swaathik <swaathik@gmail.com>
2024-08-05 16:58:37 +0530

    Change the filter UI

--- packages/protvista-filter/src/protvista-filter.js
+++ packages/protvista-filter/src/protvista-filter.js
@@ -51,33 +51,19 @@ class ProtvistaFilter extends LitElement {
         align-items: center;
         margin-top: 10px;
         line-height: 24px;
-        outline: none;
+        border-radius: 25px;
       }
 
       .protvista_checkbox_input {
         position: absolute;
-        opacity: 0;
         cursor: pointer;
         height: 0;
         width: 0;
-      }
-
-      .checkmark {
         opacity: 0.4;
-        height: 25px;
-        width: 25px;
-        background-color: #eee;
-        border-radius: 4px;
-        box-sizing: border-box;
-        border: 1px solid #bdc3c7;
-      }
-
-      .protvista_checkbox_input:checked ~ .checkmark {
-        opacity: 1;
       }
 
       .protvista_checkbox_label {
-        margin-left: 0.2rem;
+        padding: 10px;
         line-height: 1rem;
       }
     `;
@@ -109,15 +95,12 @@ class ProtvistaFilter extends LitElement {
     if (options.colors.length == null) {
       options.colors = [options.colors];
     }
+
+    /* TODO Restore the color gradient for isCompound
     const isCompound = options.colors.length > 1;
-    return html`
-      <label
-        class="protvista_checkbox ${isCompound ? "compound" : ""}"
-        tabindex="0"
-      >
-        <input
-          type="checkbox"
-          style=${`accent-color: ${
+
+    Removed:
+    style=${`background: ${
             isCompound
               ? `
             linear-gradient(${options.colors[0]},
@@ -125,6 +108,32 @@ class ProtvistaFilter extends LitElement {
           `
               : options.colors[0]
           };`}
+    */
+
+    const hexToRgba = (hex, alpha = 1) => {
+      const red = parseInt(hex.slice(1, 3), 16);
+      const green = parseInt(hex.slice(3, 5), 16);
+      const blue = parseInt(hex.slice(5, 7), 16);
+
+      return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    };
+
+    return html`
+      <label
+        class="protvista_checkbox"
+        style=${`
+          outline: ${`1px solid ${options.colors[0]}`}; 
+          background: ${
+            this.selectedFilters.has(name)
+              ? hexToRgba(options.colors[0], 0.4)
+              : "white"
+          };
+          `}
+        tabindex="0"
+      >
+        <input
+          type="checkbox"
+          class="protvista_checkbox_input"
           ?checked="true"
           .value="${name}"
           @change="${() => this.toggleFilter(name)}"
@@ -140,6 +149,7 @@ class ProtvistaFilter extends LitElement {
     } else {
       this.selectedFilters.delete(name);
     }
+
     this.dispatchEvent(
       new CustomEvent("change", {
         bubbles: true,
@@ -157,6 +167,9 @@ class ProtvistaFilter extends LitElement {
         },
       })
     );
+
+    // Re-render the component to reflect the background colour for the checkboxes
+    this.requestUpdate();
   }
 }
 
